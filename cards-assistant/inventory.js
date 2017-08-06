d990fc069c10e20a50f24a7929bd38631c3efa2a
L W <58392143a@gmail.com>
2017-08-06 09:51:28 +0200

    Fix wrong function call.

--- cards-assistant/inventory.js
+++ cards-assistant/inventory.js
@@ -3,25 +3,23 @@ const log = require('winston')
 const SteamCommunity = require('steamcommunity')
 let community = new SteamCommunity()
 
-module.exports = {
-  get (id, callback) {
+module.exports = function get (id, callback) {
+  id = new (SteamCommunity.SteamID)(id)
+  community.getSteamUser(id, (err, user) => {
+    if (err) {
+      callback(err)
+      return
+    }
+    log.info(`got profile of ${id}.`)
 
-    id = new (SteamCommunity.SteamID)(id)
-    community.getSteamUser(id, (err, user) => {
+    // Steam = 753, Community contextID = 6, tradeable only
+    user.getInventoryContents(753, 6, true, (err, inv, c, total) => {
       if (err) {
         callback(err)
         return
       }
-      log.verbose(`got profile of ${id}.`)
-
-      // Steam= 753, Community contextID = 6, tradeable only
-      user.getInventoryContents(753, 6, true, english, (err, inv, c, total) => {
-        if (err) {
-          callback(err)
-          return
-        }
-        log.verbose(`got ${total} items from ${user.name}'s inventory.`)
-      })
+      log.info(`got ${total} items from ${id}'s inventory.`)
+      callback(null, inv)
     })
-  }
+  })
 }
