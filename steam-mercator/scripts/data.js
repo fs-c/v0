a9dcec83295e367ead02af9f409f4876c1d6e16d
LW2904 <8392143a@gmail.com>
2017-08-30 17:45:21 +0200

    Cache data for 6 hours.

--- steam-mercator/scripts/data.js
+++ steam-mercator/scripts/data.js
@@ -8,18 +8,34 @@ exports.sets = getSets
 exports.parse = parseSets
 
 function getSets () {
-  return new Promise((resolve, reject) => {
-    if (fs.existsSync(PATH)) {
-      log.silly(`set_data.json found.`)
-      resolve(require(PATH)) // Bypassing the JSON.parse size limit.
-    } else {
+  function get () {
+    return new Promise((resolve, reject) => {
       request('http://cdn.steam.tools/data/set_data.json', (err, res, body) => {
         log.silly(`steam.tools CDN GET: ${res.statusCode}`)
-        if (err || res.statusCode !== 200) reject(Error(err ? err : res.statusCode))
-        fs.writeFileSync('set_data.json', body)
+        if (err || res.statusCode !== 200) reject(new Error(err ? err : res.statusCode))
+
+        let b = JSON.parse(body)
+        b.updated = Date.now()
+
+        fs.writeFileSync('set_data.json', JSON.stringify(b))
         log.silly(`set_data.json written.`)
-        resolve(JSON.parse(body))
+        resolve(JSON.parse(b))
       })
+    })
+  }
+
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(PATH)) {
+      let d = require(PATH)
+      log.silly(`set_data.json found.`)
+      if (d.updated + 6*60*60*1000 > Date.now()) {
+        resolve(d)
+      } else {
+        log.silly(`set_data.json file too old, fetching updated version.`)
+        get().then(data => resolve(data)).catch(err => reject(err))
+      }
+    } else {
+      get().then(data => resolve(data)).catch(err => reject(err))
     }
   })
 }
