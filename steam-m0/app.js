a86f9976083070dad742f4d883b370ab7e013861
LW2904 <8392143a@gmail.com>
2017-08-30 21:24:30 +0200

    Add event logs for debugging.

--- steam-m0/app.js
+++ steam-m0/app.js
@@ -3,11 +3,22 @@ const ENV  = process.env.NODE_ENV || 'prod'
 
 const ACCOUNT = require(PATH).bot
 
-require('console-stamp')(console, 'HH:MM:ss.l')
+const log = require('./logger')
 
 const Bot = require('./Bot')
 let bot = new Bot(ACCOUNT)
 
 bot.on('ready', () => {
-  console.log('bot ready.')
+  log.info('bot ready.')
 })
+
+// TODO: Figure out a way to listen to all events.
+if (ENV === 'dev') {
+  bot.on('loggedOn', () => log.debug(`loggedOn event by steam-user`))
+  bot.on('steamGuard', () => log.debug(`steamGuard event by steam-user`))
+  bot.on('disconnected', () => log.debug(`disconnected event by steam-user`))
+  bot.on('sentry', () => log.debug(`sentry event by steam-user`))
+  bot.on('webSession', () => log.debug(`webSession event by steam-user`))
+  bot.on('loginKey', () => log.debug(`loginKey event by steam-user`))
+  bot.on('tradeOffers', () => log.debug(`tradeOffers event by steam-user`))
+}
