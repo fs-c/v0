d495eadacee8fc93055971bb8babcf3b24f15d82
LW2904 <8392143a@gmail.com>
2017-08-31 14:09:56 +0200

    Loglevel now determined by NODE_ENV

--- steam-m0/logger.js
+++ steam-m0/logger.js
@@ -3,7 +3,7 @@ const moment = require('moment')
 
 logger.remove(logger.transports.Console)
 logger.add(logger.transports.Console, {
-  level: 'silly',
+  level: process.env.NODE_ENV === 'dev' ? 'silly' : 'info',
   colorize: true,
   timestamp: function() {
     return moment().format('YYYY-MM-DD hh:mm:SSA')
