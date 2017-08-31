47ccf1533e6cf8ba9e2e139ba2da5f5833615bbd
LW2904 <8392143a@gmail.com>
2017-08-31 14:09:42 +0200

    Improve 'cmd' event

--- steam-m0/components/handlers.js
+++ steam-m0/components/handlers.js
@@ -5,17 +5,18 @@ Bot.prototype.setupHandlers = function () {
 		if (!this.options.spamProtection || !this.spam(steamID, message)) {
 			// TODO: Replace sub-par parse algorithm.
 			if (message.indexOf('!') === 0) {
-				this.emit('cmd', {
-					command: message.indexOf(' ') !== -1 ? message.slice(1, message.indexOf(' ') + 1).trim() : message.slice(1),
-		      arguments: message.indexOf(' ') !== -1 ? message.slice(message.indexOf(' ') + 1).split(' ') : false
-				})
-			} else this.client.chatMessage(sid, `Couldn't parse message.`)
+				this.emit('cmd',
+					message.indexOf(' ') !== -1 ? message.slice(1, message.indexOf(' ') + 1).trim() : message.slice(1),
+		      message.indexOf(' ') !== -1 ? message.slice(message.indexOf(' ') + 1).split(' ') : false
+				)
+			} else this.client.chatMessage(steamID, `Couldn't parse message.`)
 		} else this.emit('spamMessage', steamID)
 	})
 
 	this.client.on('friendRelationship', (steamID, rel) => {
 		this.emit('relChange', steamID, rel)
-		if (rel === Steam.EFriendRelationship.RequestRecipient) {
+		// Steam.EFriendRelationship.RequestRecipient
+		if (rel === 2) {
 			this.client.addFriend(steamID, (err, name) => {
 				this.emit('addedFriend', steamID, name)
 			})
