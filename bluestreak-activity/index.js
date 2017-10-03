eb27e7f4873a9006f704f80a51ed0249d67b5fd7
LW2904 <8392143a@gmail.com>
2017-10-03 13:27:43 +0200

    Functional.

--- bluestreak-activity/index.js
+++ bluestreak-activity/index.js
@@ -1,26 +1,52 @@
-const request = require('request')
 const cheerio = require('cheerio')
 
-const log = require('../logger')
+if (!require('fs').existsSync('body.html')) {
+  const request = require('request')
+  
+  request.post({
+    url: 'http://steamcommunity.com/comment/Clan/render/103582791437945007/-1/',
+    form: {
+      start: '0',
+      count: '500'
+    }
+  }, (err, res, body) => {
+    if (err) return log.error(err)
+    parse(body.comments_html, handle)
+  })
+} else {
+  parse(require('./body').comments_html, handle)
+}
 
-// request.post({
-//   url: 'http://steamcommunity.com/comment/Clan/render/103582791437945007/-1/',
-//   form: {
-//     start: '0',
-//     count: '500'
-//   }
-// }, (err, res, body) => {
-//   if (err) return log.error(err)
-//   parse(body)
-// })
+function parse (html, cb) {
+  let comments = []
 
-let file = require('fs').readFileSync('comments_html.html', { encoding: 'utf-8' })
+  const $ = cheerio.load(html)
 
-// function parse (body) {
-//   try { body = JSON.parse(body) } catch (e) { return log.error(e) }
-//   const $ = cheerio.load(body.comments_html)
-//
-//   $('.commentthread_comment_content').each(i => {
-//     console.log($(this).children('.commentthread_comment_text').text())
-//   })
-// }
+  $('.commentthread_comment').each((i, e) => {
+    const $content = $(e).children('.commentthread_comment_content')
+    const $author = $content.children('.commentthread_comment_author')
+
+    let id = $content.children('.commentthread_comment_text').attr('id').slice(16).trim()
+
+    let text = $content.children('.commentthread_comment_text').text().trim()
+    let href = $author.children('.commentthread_author_link').attr('href')
+
+    let author = {
+      name: $author.children('.commentthread_author_link').text().trim(),
+      vanityURL: href.indexOf('/profiles/') === -1 ? href.slice(29) : undefined,
+      id: href.indexOf('/profiles/') === -1 ? undefined : href.slice(36)
+    }
+
+    comments.push({
+      id,
+      author,
+      text
+    })
+  })
+
+  cb(comments)
+}
+
+function handle (comments) {
+  console.log(`Got total of ${comments.length} comments.`)
+}
