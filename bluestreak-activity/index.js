be6af59b32d1ac4257584951573615a4fd2f27f8
LW2904 <8392143a@gmail.com>
2017-10-03 21:13:01 +0200

    Promisify.

--- bluestreak-activity/index.js
+++ bluestreak-activity/index.js
@@ -1,52 +1,58 @@
 const cheerio = require('cheerio')
+const request = require('request')
 
-if (!require('fs').existsSync('body.html')) {
-  const request = require('request')
-  
-  request.post({
-    url: 'http://steamcommunity.com/comment/Clan/render/103582791437945007/-1/',
-    form: {
-      start: '0',
-      count: '500'
-    }
-  }, (err, res, body) => {
-    if (err) return log.error(err)
-    parse(body.comments_html, handle)
+// BLS: 103582791437945007
+
+get('103582791437945007', '0', '200')
+.catch(err => console.error(`ERROR GETTING COMMENTS: ${err}`))
+.then(parse).then(comments => {
+  console.log(comments)
+})
+
+function get (id, start, count, cb) {
+  return new Promise((resolve, reject) => {
+    request.post({
+      url: `http://steamcommunity.com/comment/Clan/render/${id}/-1/`,
+      form: { start, count }
+    }, (err, res, body) => {
+      if (err) reject(err)
+      // Write res body to disk and get it again to take advantage of require()s parsing,
+      // remove file after callback.
+      require('fs').writeFileSync('body.json', body)
+      resolve(require('./body.json').comments_html)
+      require('fs').unlinkSync('body.json')
+    })
   })
-} else {
-  parse(require('./body').comments_html, handle)
 }
 
 function parse (html, cb) {
-  let comments = []
+  return new Promise((resolve, reject) => {
+    let comments = []
 
-  const $ = cheerio.load(html)
+    const $ = cheerio.load(html)
 
-  $('.commentthread_comment').each((i, e) => {
-    const $content = $(e).children('.commentthread_comment_content')
-    const $author = $content.children('.commentthread_comment_author')
+    $('.commentthread_comment').each((i, e) => {
+      const $content = $(e).children('.commentthread_comment_content')
+      const $author = $content.children('.commentthread_comment_author')
 
-    let id = $content.children('.commentthread_comment_text').attr('id').slice(16).trim()
+      let id = $content.children('.commentthread_comment_text').attr('id').slice(16).trim()
 
-    let text = $content.children('.commentthread_comment_text').text().trim()
-    let href = $author.children('.commentthread_author_link').attr('href')
+      let text = $content.children('.commentthread_comment_text').text().trim()
+      let href = $author.children('.commentthread_author_link').attr('href')
 
-    let author = {
-      name: $author.children('.commentthread_author_link').text().trim(),
-      vanityURL: href.indexOf('/profiles/') === -1 ? href.slice(29) : undefined,
-      id: href.indexOf('/profiles/') === -1 ? undefined : href.slice(36)
-    }
+      let author = {
+        name: $author.children('.commentthread_author_link').text().trim(),
+        vanityURL: href.indexOf('/profiles/') === -1 ? href.slice(29) : false,
+        id: href.indexOf('/profiles/') === -1 ? undefined : href.slice(36)
+      }
 
-    comments.push({
-      id,
-      author,
-      text
+      comments.push({
+        id,
+        author,
+        text
+      })
     })
+
+    resolve(comments)
   })
-
-  cb(comments)
-}
-
-function handle (comments) {
-  console.log(`Got total of ${comments.length} comments.`)
 }
