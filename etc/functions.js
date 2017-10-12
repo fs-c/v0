ec63fd87ac163cd334a682d2bb21898ee297ed3d
LW2904 <8392143a@gmail.com>
2017-10-12 18:05:35 +0200

    Add TODO ideas.

--- etc/functions.js
+++ etc/functions.js
@@ -1,3 +1,11 @@
+/**
+*   TODO:
+*     - Fibonacci numbers.
+*     - Largest prime factor. e.g.: 5 * 7 * 13 * 29 = 13195 - solution: 29
+*     - Largest palindrome number made from the product of two 3-digit numbers.
+*     - What is the smallest positive number that is evenly divisible by all of the numbers from x to y?
+*/
+
 // Determines if string a is an anagram of string b.
 const anagram = ([...a], [...b]) => a.sort() == b.sort() + ''
 
@@ -20,4 +28,4 @@ const fizzbuzz = (a = []) => {
 // Returns the faculty of integer x.
 const faculty = x => x === 1 ? 1 : x * faculty(x - 1)
 
-console.log(faculty(10))
+console.log(reverse('reverse'))
