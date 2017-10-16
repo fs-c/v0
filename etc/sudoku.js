5830a0904e83a55948c16444082c6e3ad250a437
fsoc <admin@fsoc.space>
2017-10-16 16:49:35 +0200

    Playing around with sudoku.

---
+++ etc/sudoku.js
@@ -0,0 +1,16 @@
+let sudoku = [ 0, 4, 0, 0, 8, 3, 0, 6, 0, 6, 0, 7, 0, 0, 0, 4, 0, 0, 3, 0, 0, 0, 6, 0, 2, 9, 0, 0, 0, 4, 0, 1, 0, 9, 2, 6, 9, 0, 0, 0, 3, 0, 0, 0, 8, 8, 2, 6, 0, 7, 0, 5, 0, 0, 0, 7, 9, 0, 5, 0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 3, 0, 9, 0, 6, 0, 1, 9, 0, 0, 7, 0 ]
+let sudoku_2d = parse2d(sudoku)
+
+function parse2d (sudoku) {
+  
+}
+
+function solved (sudoku_2d) {
+
+}
+
+function solve (sudoku) {
+  while (!solved(sudoku)) { }
+}
+
+console.log(solved(sudoku))
