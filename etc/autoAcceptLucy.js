20e2abb685dd3b04f8453dd54f5f0bf50fd6da07
LW2904 <8392143a@gmail.com>
2017-10-20 21:02:05 +0200

    Fix DATA bug.

--- etc/autoAcceptLucy.js
+++ etc/autoAcceptLucy.js
@@ -1,4 +1,4 @@
-const PATH = process.env.DATA_PATH || '../../steamdata'
+const DATA = require(process.env.DATA_PATH || '../../steamdata')
 
 const Trader = require('../../v1/steam-trade-assistant/Trader.js')
 const log = require('../logger')
@@ -7,8 +7,8 @@ let trader = new Trader(DATA['main'])
 
 // Basic logging.
 trader.on('ready', () => log.info(`trader ready.`))
-trader.on('clientError', (err, res) => log.error(`[${name}] error while logging in: ${res || err}`))
-trader.on('managerError', err => log.error(`[${name}] trade manager error: ${err.message || err}`))
+trader.on('clientError', (err, res) => log.error(`error while logging in: ${res || err}`))
+trader.on('managerError', err => log.error(`trade manager error: ${err.message || err}`))
 
 trader.on('newOffer', offer => {
   log.verbose(`new offer ${offer.id}.`)
@@ -19,7 +19,7 @@ trader.on('newOffer', offer => {
 
     // If item is the karambit.
     if (offer.itemsToGive[0].id === '12368203069') {
-      trader.accept(offer).then(() => log.info(`accepted offer.`))
+      trader.accept(offer).then(() => log.info(`accepted offer by lucy for karambit.`))
     }
   }
 })
