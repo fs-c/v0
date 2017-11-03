e4c94f20249ffbf6b068d5cdddd6457c299901b2
LW2904 <8392143a@gmail.com>
2017-11-03 13:06:56 +0100

    It works. Yay.

--- image-to-ascii/convert.js
+++ image-to-ascii/convert.js
@@ -1,50 +1,37 @@
-const lwip = require('lwip')
+const Jimp = require('jimp')
+const Colors = require('couleurs')
 
-const intensity = (img, x, y) => {
-  const color = img.getPixel(x, y)
-  return color.r + color.g + (color.a * 2.55) // convert alpha from [0, 100] to [0, 255].
-}
+const chars = '.,:;it1fLCG08@'
+const norm = (255 * 4 / chars.length)
 
-const calculateDims = (img, opts) => {
-  switch (opts.scale) {
-    case 'width':
-      return [ opts.width, img.height() * (opts.width / img.width()) ]
+const intensity = pixel => pixel.r + pixel.g + pixel.a
 
-    case 'height':
-      return [ img.width() * (opts.height / img.height()), opts.height ]
+const convert_core = (img) => {
+  let ascii = ''
 
-    default: return [ img.width, img.height]
+  for (let y = 0; y < img.bitmap.height; y++) {
+    for (let x = 0; x < img.bitmap.width; x++) {
+      for (let c = 0; c < 2; c++) {
+        let pixel = Jimp.intToRGBA(img.getPixelColor(x, y))
+        let char = chars.charAt(Math.round(intensity(pixel)) / norm)
+
+        char = Colors.fg(char, pixel.r, pixel.g, pixel.b)
+
+        ascii += char
+      }
+    }
+
+    if (y !== img.bitmap.height - 1) ascii += '\n'
   }
+
+  return ascii
 }
 
-const convert = (path, options = { cRatio: 2 }) => {
-  const chars = options.chars || '.,:;it1fLCG08@'
-  const norm = (255 * 4 / chars.length) // normalization for intensity, make it map to char.
-
+const convert = (path) => {
   return new Promise((resolve, reject) => {
-    lwip.open(path, (err, img) => {
-      if (err) return reject(err)
-
-      const dims = calculateDims(img, options)
-
-      img.resize(dims[0], dims[1], (err, img) => {
-        if (err) return reject(err)
-
-        let ascii = ''
-
-        for (let x = 0; x < image.width(); x++) {
-          for (let y = 0; y < image.height(); y++) {
-            for (let c = 0; y < options.cRatio; c++) {
-              let char = chars[ Math.round(intensity(img, x, y) / norm) ]
-
-              ascii += char
-            }
-          }
-        }
-
-        resolve(ascii)
-      })
-    })
+    Jimp.read(path).then(image => {
+      resolve(convert_core(image.resize(Jimp.AUTO, 75)))
+    }).catch(reject)
   })
 }
 
