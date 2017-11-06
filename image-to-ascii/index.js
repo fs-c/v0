60e04d0fe962bb701534db626fb582bd3c0f152b
LW2904 <8392143a@gmail.com>
2017-11-06 17:24:58 +0100

    Move projects to v1.

--- image-to-ascii/index.js
+++
@@ -1,3 +0,0 @@
-const convert = require('./convert')
-
-convert('./faz.jpg').then(console.log).catch(console.error)
--- markov/index.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const MarkovChain = require('./markov')
-
-const chain = new MarkovChain(fs.readFileSync('data.txt', 'utf8'))
-
-console.log(chain.process('Interactive', 5))
