2e1b813737497c9620dfa0d3cf4bfbf1234783c7
fsoc <8392143a@gmail.com>
2017-11-13 16:47:33 +0100

    Fixes.

--- upload/server.js
+++ upload/server.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 const express = require('express')
 const app = express()
 
@@ -6,17 +8,30 @@ app.enable('trust proxy')
 app.use(require('helmet')()) // Basic security.
 app.use(require('morgan')('dev')) // Logging.
 
+app.use(express.static('files'))
 app.use(express.static('static'))
 
 const fileUpload = require('express-fileupload')
 app.use(fileUpload({ limits: { fileSize: 1024 * 1024 } })) // 1 MiB
 
+const cleanup = (maxNum = 2) => {
+  const names = fs.readdirSync('./files')
+    .map(n => parseInt(n.slice(0, n.lastIndexOf('.jpg')), 10))
+    .sort((a, b) => a - b)
+
+  if (names.length >= maxNum)
+    for (const name of names.slice(0, names.length - maxNum))
+      fs.unlinkSync(`./files/${name}.jpg`)
+}
+
 app.post('/upload', (req, res) => {
   if (!req.files)
     return res.status(400).send('No files were uploaded.')
 
-  let file = req.files.file
-  let name = Date.now() + file.name.slice(file.name.lastIndexOf('.'))
+  cleanup()
+
+  const file = req.files.file
+  const name = Date.now() + file.name.slice(file.name.lastIndexOf('.'))
 
   file.mv('./files/' + name, err => {
     if (err) return res.status(500).send(err)
@@ -25,4 +40,4 @@ app.post('/upload', (req, res) => {
   })
 })
 
-app.listen(8083)
+app.listen(process.env.PORT || 8083)
