a64f046b26a867ed1fc315d7405a45c9bc40a04d
fsoc <8392143a@gmail.com>
2017-11-16 18:33:35 +0100

    Barebones commit.

---
+++ etc/redeemer.js
@@ -0,0 +1,25 @@
+const log = require('../logger')
+
+const User = require('steam-user')
+const user = new User()
+
+const account = {
+  accountName: 'mrlaurinator',
+  password: '2904Lauri!'
+}
+
+const parse = string => string.split('\n').map(e => e.replace('\r', ''))
+const keys = parse(require('fs').readFileSync('./keys.txt', 'utf8'))
+
+const callback = (result, details, packages) => {
+  log.debug(`result: ${User.EResult[result]}/${result} details: ${User.EPurchaseResult[details]}/${details}`)
+
+  if (result === User.ERsult.OK) log.info(`successfully redeemed key`)
+}
+
+user.logOn(account)
+
+user.on('loggedOn', () => {
+  let delay = 0
+  keys.map(key => setTimeout(user.redeemKey, delay++ * 30000, key, callback))
+})
