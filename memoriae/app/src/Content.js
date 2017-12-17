3c606abaf24393269df3e6e7844c00502f05dcda
fsoc <8392143a@gmail.com>
2017-12-17 06:49:33 +0100

    Eject.

--- memoriae/app/src/Content.js
+++ memoriae/app/src/Content.js
@@ -3,11 +3,7 @@ import React, { Component } from 'react'
 export default class Content extends Component {
   render() {
     return (
-      <div class="card content">
-        <div class="card-body">
-          
-        </div>
-      </div>
+      <div></div>
     )
   }
 }
--- memoriae/app/src/index.js
+++ memoriae/app/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 import registerServiceWorker from './registerServiceWorker'
+
+import App from './App'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
