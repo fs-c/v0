a78546bc19b64cc8ae95e04aeedda29984abeaf6
fsoc <8392143a@gmail.com>
2018-01-06 17:16:51 +0100

    Implement basic functionality.

--- steam-chat/index.js
+++ steam-chat/index.js
@@ -1,16 +1,67 @@
 const User = require('steam-user')
+const SteamID = require('steamid')
+
+let dictionary = {  }
+
+const totp = require('steam-totp')
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  completer: (line, callback) => {
+    const hits = Object.values(dictionary)
+      .map(e => e.name).filter(e => e.startsWith(line))
+        
+    callback(null, [ hits, line ])
+  }
+})
 
 const log = require('./components/logger')
-const account = require('./scripts/getAccount')()
 
 let client = new User()
+let account = require('./scripts/getAccount')()
 
 client.setOption('promptSteamGuardCode', false)
-
 require('./components/logEvents')(client)
-
-console.log(account)
 
 client.logOn(account)
 
-client.on('loggedOn', () => log.event('logged on'))
+client.on('steamGuard', (email, callback) => {
+  if (!email && account.shasec)
+    return callback(totp.getAuthCode(account.shasec))
+
+  console.log(`${email ? 'Email' : 'Mobile'} code: `)
+  rl.on('line', line => callback(line.trim()))
+})
+
+client.on('loggedOn', () => {
+  client.setPersona(1)
+
+  log.info('ready')
+
+  require('./components/buildDictionary')(client.steamID.getSteamID64())
+    .then(d => {
+      log.debug(`dictionary ready`)
+      dictionary = d
+    }).catch(e => { throw log.error('failed to build dictionary', e) })
+})
+
+client.on('friendMessage', (senderID, message) => {
+  log.chat(`${dictionary[senderID.toString()].name} >> ${message}`)
+})
+
+rl.on('line', input => {
+  log.verbose(`got new line "${input}"`)
+
+  if (input.indexOf('<<') === -1) return
+
+  let recipient = input.slice(0, input.indexOf('<<')).trim()
+  let message = input.slice(input.indexOf('<<') + 2).trim()
+
+  let id = Object.values(dictionary)
+    .filter(e => e.customURL === recipient || e.name === recipient)[0]
+    .steamID
+
+  log.debug(`sending message ${message} to ${recipient} / ${id.toString()}`)
+
+  client.chatMessage(id, message)
+})
