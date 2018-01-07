813304f98e8fb961a84bc9a26408748c087e9e5f
fsoc <8392143a@gmail.com>
2018-01-07 01:40:41 +0100

    Use fallback when no args specified.

--- steam-chat/scripts/getAccount.js
+++ steam-chat/scripts/getAccount.js
@@ -1,6 +1,6 @@
 const parse = module.exports = () => {
   const args = process.argv.slice(2)
-    .map((e, i) => !(i % 2) ? e.slice(1) : e)
+    .map((e, i) => !(i % 2) ? e.slice(e.lastIndexOf('-')) : e)
     .reduce((acc, val, i, a) => {
       if (!(i % 2)) acc[val] = a[i + 1]
       return acc
@@ -14,5 +14,6 @@ const parse = module.exports = () => {
   if (args.account && require('fs').existsSync('../../steamdata.json'))
     return require('../../../steamdata.json')[args.account]
 
-  throw new Error('No account specified.')
+  require('../components/logger').warn('no args provided, assuming defaults')
+  return require('../../../steamdata.json').main
 }
