4b0dec697df4d11a98ca1007f6065b93bd0e8836
fsoc <8392143a@gmail.com>
2018-01-10 10:57:08 +0100

    Handle unexpected return body.

--- steam-chat/scripts/getFriends.js
+++ steam-chat/scripts/getFriends.js
@@ -5,17 +5,24 @@ const request = require('request')
 const log = require('../components/logger')
 
 const getFriends = module.exports = id => {
-  return new Promise((resolve, reject) => {
-    request(`${URL}ISteamUser/GetFriendList/v0001/` +
-      `?key=${global.API_KEY}&steamid=${id}&relationship=friend`, 
-      (err, res, body) => {
-        if (err) 
-          return reject(err)
+  let url = `${URL}ISteamUser/GetFriendList/v0001/` +
+    `?key=${global.API_KEY}&steamid=${id}&relationship=friend`
 
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) 
+        return reject(err)
+
+      try {
+        // This will cause an exception if key invalid or profile private.
+        let friends = JSON.parse(body).friendslist.friends
         log.debug(`got friend list of user ${id}`)
 
-        resolve(JSON.parse(body).friendslist.friends)
+        resolve(friends)
+      } catch(e) {
+        log.warn(`couldn't get friendslist, you should probably abort`)
+        reject(e)
       }
-    )
+    })
   })
 }
