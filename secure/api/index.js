ac345a48878431529c3dcefcddabd8b3d317d38b
fsoc <8392143a@gmail.com>
2018-01-14 00:02:15 +0100

    Rework API.

--- secure/api/index.js
+++ secure/api/index.js
@@ -1,4 +1,18 @@
-module.exports = {
-  getCodes: require('./getCodes'),
-  getAccounts: require('./getAccounts')
-}
+const routes = [
+  {
+    path: '/accounts',
+    name: 'getAccounts',
+    description: 'returns all accounts',
+    level: -1
+  },
+  {
+    path: '/codes',
+    name: 'getCodes',
+    description: 'returns steam auth codes for all accounts'
+  }
+]
+
+module.exports = routes.map(e => {
+  e.function = require('./' + e.name)
+  return e
+})
--- secure/routes/api/index.js
+++ secure/routes/api/index.js
@@ -1,32 +1,18 @@
 const api = require('../../api/')
 const router = module.exports = require('express').Router()
 
-const functions = [
-  {
-    route: '/accounts',
-    name: 'getAccounts',
-    description: 'returns all accounts in .steam.json',
-    level: -1
-  },
-  {
-    route: '/codes',
-    name: 'getCodes',
-    description: 'returns steam auth codes for all accounts in .steam.json'
-  }
-]
+router.get('/', (req, res) => res.render('api', { api }))
 
-router.get('/', (req, res) => res.render('api', { functions }))
-
-functions.map(func => {
-  router.get(func.route, (req, res, next) => {
-    if (func.level && func.level < req.user.access)
+api.map(route => {
+  router.get(route.path, (req, res, next) => {
+    if (route.level && route.level < req.user.access)
       return next(
         new Error(`Not authorized to run this function `
           + `(${func.level} < ${req.user.access})`)
       )
 
     try {
-      api[func.name]()
+      route.function()
         .then((...ret) => res.json({ status: 'success', data: ret }))
         .catch(err => next(err))
     } catch(e) { next(e) }
