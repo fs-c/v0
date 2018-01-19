280225c9aa0aac394564cb23cb4a4e4f71afa8c9
fsoc <8392143a@gmail.com>
2018-01-19 10:42:09 +0100

    Some tests.

---
+++ etc/debug.js
@@ -0,0 +1,7 @@
+const debug = require('debug')
+
+const a = debug('a')
+const b = debug('b')
+
+a('a')
+b('b')
---
+++ etc/smurf.js
@@ -0,0 +1,10 @@
+let lever = false
+
+let i = 0
+while (true) {
+  i++
+
+  
+}
+
+console.log(`${i === 99 ? 'correct' : 'nope'}`)
