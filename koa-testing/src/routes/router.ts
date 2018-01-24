b00e135f99d16c651304d7e6334f04a39485fd5f
fsoc <8392143a@gmail.com>
2018-01-24 22:36:05 +0100

    Add redirect functions and render index if authed.

--- koa-testing/src/routes/router.ts
+++ koa-testing/src/routes/router.ts
@@ -1,11 +1,44 @@
+import * as fs from 'fs';
+import * as log from 'debug';
+import * as path from 'path';
 import * as Router from 'koa-router';
+
+const debug = log('app:router');
 
 const router = new Router();
 export default router;
 
+type INext = () => Promise<any>;
+
+export async function ifLoggedOn(ctx: Router.IRouterContext, next: INext) {
+  if (ctx.isAuthenticated()) {
+    await next();
+  } else {
+    ctx.redirect('/login');
+  }
+}
+
+export async function redirectIfLoggedOn(
+  ctx: Router.IRouterContext,
+  next: INext,
+) {
+  if (ctx.isAuthenticated()) {
+    ctx.redirect('/');
+  } else {
+    await next();
+  }
+}
+
 router.get('/', async (ctx, next) => {
-  ctx.type = 'html';
-  ctx.body = 'Sometimes I don\'t sleep. </br><a href="/login">Log in.</a>';
+  if (ctx.isAuthenticated()) {
+    await ctx.render('index');
+  } else {
+    ctx.status = 200;
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream(
+      path.join(process.cwd(), 'src/public/index.html')
+    );
+  }
 });
 
 import userActions from './userActions';
