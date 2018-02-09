673dc0d2d0f9eff6c4c020a1664e439f7fedf3e6
fsoc <8392143a@gmail.com>
2018-02-09 14:23:49 +0100

    Commit project.

---
+++ steam-get2fa/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const Steam = require('steam-user');
+const rls = require('readline-sync');
+
+const account = {
+  accountName: rls.question('Name: '),
+  accountName: rls.question('Pass: '),
+}
+
+const user = new Steam();
+
+user.logOn(account);
+
+user.on('loggedOn', () => {
+  console.log('logged on');
+
+  user.enableTwoFactor((response) => {
+    console.log('enabled two factor, response: ');
+    console.log(response);
+
+    fs.writeFileSync('response.json', JSON.stringify(response), 'utf8');
+
+    const code = rls.question('SMS Code: ');
+
+    user.finalizeTwoFactor(response.shared_secret, code, (err) => {
+      if (err) throw err;
+
+      console.log('finalized two factor');
+    })
+  });
+});
