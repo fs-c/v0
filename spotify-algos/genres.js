75143c683d1b7d80b1eaf0d01dcde536e003b9d9
fsoc <8392143a@gmail.com>
2018-02-21 22:29:16 +0100

    Play around with spotify API data.

---
+++ spotify-algos/genres.js
@@ -0,0 +1,16 @@
+const albums = require('./albums');
+
+// Set of all unique genres of all artists.
+const genresSet = new Set(
+  albums.map((album) => album.genres)
+        .reduce((acc, cur) => acc.concat(cur), [])
+);
+
+// Object of genres, key is the name and value the number of occurences.
+const genresObj = albums.reduce((acc, cur) => {
+  cur.genres.forEach((genre) => 
+    acc[genre] = acc[genre] ? acc[genre] + 1 : 1
+  )
+
+  return acc;
+}, {});
