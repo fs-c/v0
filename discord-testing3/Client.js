84b1b6267dd085e5e4d8e75a295adb0778d057fc
fsoc <8392143a@gmail.com>
2018-03-04 20:34:02 +0100

    All kinds of minor stuff.

--- discord-testing3/Client.js
+++ discord-testing3/Client.js
@@ -1,16 +1,17 @@
 const WS_URL = 'wss://gateway.discord.gg/?v=6&encoding=json';
 
 const WebSocket = require('ws');
-const { EventEmitter } = require('events');
-const debug = require('debug')('app');
+const EventEmitter = require('events');
+const debug = require('debug')('client');
 
-class Client extends EventEmitter {
+const Client = exports.Client = class extends EventEmitter {
   constructor(token) {
     super();
 
-    // All values which default to undefined will be set in the process 
-    // of connecting, do not count on them being set though.
+    // All values which default to undefined may be set in the process 
+    // of connecting, do not count on them being set at any point in time.
 
+    this.socket;
     this.token = token;
 
     this.connection = {
@@ -29,17 +30,21 @@ class Client extends EventEmitter {
   }
 
   connect() { return new Promise((resolve, reject) => {
+    debug('connecting');
+
     this.socket = new WebSocket(WS_URL);
 
     this.socket.on('ready', resolve);
-    this.socket.on('message', this.handleSocket);
+    this.socket.on('message', (data) => this.handleSocket(data));
   })}
 
-  handleSocket(data) {
+  handleSocket(raw) {
     debug('received message');
 
-    const message = JSON.parse(data);
+    const message = JSON.parse(raw);
     const data = message.d;
+
+    debug(data);
 
     switch (message.op) {
       // Dispatch.
@@ -60,30 +65,37 @@ class Client extends EventEmitter {
         debug('socket hello');
 
         if (this.sequence && this.sessionID) {
+          debug('resuming');          
+
           // We were already connected before, resume connection.
-          client.socket.send(this.payloads.resume());
+          this.socket.send(this.payloads.resume());
         } else {
+          debug('identifying %o', this.payloads.identify());
+
           // Initial connect, we have to identify ourselves.
-          client.socket.send(this.payloads.identify());
+          this.socket.send(this.payloads.identify());
         }
 
-        if (Stop.heartbeat) {
+        if (this.heartbeat.timer) {
           // Clear the heartbeat timer if it exists.
-          clearInterval(heartbeat);
+          clearInterval(this.heartbeat.timer);
         }
 
         // Try to get the interval from sent data, fallback to default if 
         // it's not provided (rare).      
-        if (this.data.heartbeat_interval) {
-          this.heartbeat.interval = this.data.heartbeat_interval;
+        if (data.heartbeat_interval) {
+          this.heartbeat.interval = data.heartbeat_interval;
         }
+
+        debug('sending heartbeats with %oms interval',
+          this.heartbeat.interval);
 
         // Send periodic heartbeats.
         this.heartbeat.timer = setInterval(() => {
           this.heartbeat.last = Date.now(); // To measure our ping.
 
-          // This will get cleared in the heartbeat ACK, if it doesn't 
-          // we're in trouble.
+          // This will get cleared in the heartbeat ACK; if it doesn't, 
+          // we've exceeded our ping limit and should close the connection.
           this.heartbeat.timeout = setTimeout(() => {
             this.socket.close(4000, 'No heartbeat received.');
           }, this.heartbeat.maxDelay);
@@ -116,11 +128,11 @@ class Client extends EventEmitter {
 }
 
 Client.prototype.payloads = {
-  heartbeat: () => ({
+  heartbeat: function() { JSON.stringify({
     op: 1,
-    d: client.connecttion.sequence,
-  }),
-  identify: () => ({
+    d: this.connection.sequence,
+  })},
+  identify: () => JSON.stringify({
     op: 2,
     d: {
       token: this.token,
@@ -134,7 +146,7 @@ Client.prototype.payloads = {
       }
     }
   }),
-  resume: () => ({
+  resume: () => JSON.stringify({
     op: 6,
     d: {
       seq: this.sequence,
