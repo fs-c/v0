b311b8b35d71b601891cd04b8dcdadcc04e95807
fsoc <8392143a@gmail.com>
2018-03-13 10:15:23 +0100

    Redo config and token reading.

--- discord-stream/index.js
+++ discord-stream/index.js
@@ -4,32 +4,23 @@ const moment = require('moment');
 const Discord = require('../discord-client/Client');
 const args = require('minimist')(process.argv.slice(2));
 
-const { join } = require('path');
-const { homedir } = require('os');
-const { existsSync } = require('fs');
+const configPath = args.config 
+  || require('path').join(require('os').homedir, '.discord.json');
 
-let token;
+const config = require('fs').existsSync(configPath) 
+  ? require(configPath) : undefined;
 
-// TODO: Ugly.
-if (args.token) {
-  token = args.token;
-} else if (args.config) {
-  try {
-    token = require(args.config).token;
-  } catch (e) { throw e }
-} else if (existsSync(join(homedir(), '.discord.json'))) {
-  try {
-    token = require(join(homedir(), '.discord.json')).token;
-  } catch(e) { throw e }
-} else if (process.env.token) {
-  token = process.env.token;
-}
+const token = args.token || process.env.token || config.token;
 
 if (!token) {
   throw new Error('No token provided.');
 }
 
 const client = new Discord.Client(token);
+
+client.connect()
+  .then(() => console.log('connected'))
+  .catch(console.error);
 
 const log = (message) => {
   console.log(
@@ -38,10 +29,6 @@ const log = (message) => {
     + `${message.author.webhook_id || message.author.username}: `
     + `${message.content}`
   );
-};
-
-client.connect()
-  .then(() => console.log('connected'))
-  .catch(console.error)
+}
 
 client.on('message', log);
