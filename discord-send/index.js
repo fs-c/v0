22e9775f73173b7d4cd00ea6268ebb3d8a6c3a62
fsoc <8392143a@gmail.com>
2018-03-13 15:12:58 +0100

    Initial partial code.

---
+++ discord-send/index.js
@@ -0,0 +1,18 @@
+const Discord = require('../discord-client');
+
+const client = new Discord.Client();
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  completer: (line, callback) => {
+    const hits = [];
+
+    callback(null, [ hits, line ]);
+  }
+});
+
+rl.on('line', (line) => {
+  
+});
