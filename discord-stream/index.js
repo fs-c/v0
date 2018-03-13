cacd2a2392b929a8a6921a989afe4e6f40f6bb2a
fsoc <8392143a@gmail.com>
2018-03-13 10:16:55 +0100

    Import client from index.

--- discord-stream/index.js
+++ discord-stream/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const moment = require('moment');
-const Discord = require('../discord-client/Client');
+const Discord = require('../discord-client/');
 const args = require('minimist')(process.argv.slice(2));
 
 const configPath = args.config 
