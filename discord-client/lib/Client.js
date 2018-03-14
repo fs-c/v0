cab513d8394de3c79ec2a7a2e642cfaf5c40a13a
fsoc <8392143a@gmail.com>
2018-03-14 17:18:12 +0100

    Major client rework.

--- discord-client/lib/Client.js
+++ discord-client/lib/Client.js
@@ -1,205 +1,91 @@
-const WS_VER = process.env.WS_VER || '6';
-const WS_URL = `wss://gateway.discord.gg/?v=${WS_VER}&encoding=json`;
-
+const forward = require('fwd');
 const WebSocket = require('ws');
 const EventEmitter = require('events');
 const debug = require('debug')('discord');
 
 const { payloads } = require('./payloads');
 
-const Client = module.exports = class extends EventEmitter {
-  constructor(token) {
-    super();
+require('debug').enable('discord');
 
+const Client = module.exports = class extends EventEmitter {
+  constructor(token, options = {}) {
     if (!token) {
-      throw new Error('No token!');
+      throw new Error('No token provided.');
     }
 
-    // All values which default to undefined may be set in the process 
-    // of connecting, do not count on them being set at any point in time.
+    options.version = options.version || 6;
+
+    super();
 
     this.socket;
-    this.token = token;
 
-    this.state = 'dormant';
+    this.token = token;
+    this.readyState = 'disconnected';
+
+    this.gateway = {
+      version: options.version,
+      url: `wss://gateway.discord.gg/?v=${options.version}&encoding=json`,
+    }
 
     this.connection = {
-      sequence: 0,
-      ping: undefined,
-      sessionID: undefined,
+      sequence: 0,              // Allow for persistence across disconnects.
+      ping: undefined,          // Ms delay between heartbeat and ACK.
+      sessionID: undefined,     // Gotten from 'READY' event.
     }
 
     this.heartbeat = {
-      last: 0,
-      timer: undefined,         // Periodic heartbeats.
-      timeout: undefined,       // Check for timeout.
-      interval: 15 * 1000,      // Most likely overwritten by server response.
-      maxDelay: 15 * 1000,      // Max delay for heartbeat response.
-    }
-
-    this.reconnect = {
-      timer: undefined,
-      delay: 1000 * 60,
+      lastSent: 0,              // Last sent heartbeat, to calculate ping.
+      timer: undefined,         // The heartbeat timer.
+      interval: 15 * 1000,      // Heartbeat interval; this will be overwritten.
+      maxDelay: 15 * 1000,      // Max ms delay between heartbeat and ACK.
+      timeoutTimer: undefined,  // Detect dead or zombied connection.
     }
   }
 
-  get payloads() {
-    return payloads.bind(this)();
+  get state() { return this.readyState }
+  set state(state) {
+    debug('state %o -> %o', this.readyState, state);
+
+    this.emit('status', this.readyState, state);
+
+    this.readyState = state;
   }
 
-  set state(updated) {
-    debug(`changed state from %o to %o`, this.state, updated);    
-
-    this.state = updated;
-  }
-
-  connect() { return new Promise((resolve, reject) => {
-    this.state = this.reconnect.timer ? 'reconnecting' : 'connecting';
-
-    if (this.reconnect.timer) {
-      this.reconnect.timer = undefined;
+  connect() {
+    if (this.state !== 'disconnected') {
+      return new Error(`Invalid state (${this.state})!`);
     }
 
-    this.socket = new WebSocket(WS_URL);
+    this.state = 'connecting';
 
-    this.socket.on('message', (data) => this.handleSocket(data));
+    this.socket = new WebSocket(this.gateway.url);
+
+    this.socket.on('message', this.handle);
 
     this.socket.on('open', () => {
-      resolve();
-
       this.state = 'connected';
     });
 
-    this.socket.on('close', (code, reason) => {
-      debug('connection closed: %o (%o)', code, reason)
-
-      this.kill();
-
-      debug('reconnecting in %o', this.reconnect.delay);
-      this.reconnect.timer = setTimeout(this.connect, this.reconnect.delay);
+    this.socket.on('close', () => {
+      this.state = 'disconnected';
     });
 
-    this.socket.on('error', (err) => {
-      this.state = 'errored';
-
-      debug('encountered error %o', err.message);
-    });
-  })}
-
-  // Clear any timers, reset defaults.
-  kill() {
-    clearInterval(this.heartbeat.timer);
-    this.heartbeat.timer = undefined;
-    clearInterval(this.heartbeat.timeout);
-    this.heartbeat.timeout = undefined;
-
-    clearInterval(this.reconnect.timer);
-    this.reconnect.timer = undefined;
-
-    debug('cleared timers');
-
-    if (this.socket.readyState <= 1) {
-      this.socket.close();
-      this.state = 'closed';      
-    }
+    // Forward error, let user decide what to do with the information.
+    this.socket.on('error', (err) => this.send('error', err));
   }
 
   send(data) {
-    if (!this.socket || this.socket.readyState !== 1) {
+    if (this.socket || this.socket.readyState !== 1) {
       return;
     }
 
     this.socket.send(data, (err) => {
-      if (err)
+      debug('failed sending message (%o)', err.message);
+
+      this.emit('error', err)
     });
   }
-
-  handleSocket(raw) {
-    const message = JSON.parse(raw);
-    const data = message.d;
-
-    debug('received message (%o)', message.t || message.op);    
-
-    switch (message.op) {
-      // Dispatch.
-      case 0: this.sequence++;
-        break;
-      // Invalid session.
-      case 9:
-        debug('invalid session');
-
-        this.connect.sequence = 0;
-        this.connection.sessionID = null;
-
-        this.send(4000, 'Received an invalid session ID.')
-
-        break;
-      // Hello.
-      case 10:
-        debug('socket hello');
-
-        if (this.sequence && this.sessionID) {
-          debug('resuming');          
-
-          // We were already connected before, resume connection.
-          this.send(this.payloads.resume);
-        } else {
-          debug('identifying');
-
-          // Initial connect, we have to identify ourselves.
-          this.send(this.payloads.identify);
-        }
-
-        if (this.heartbeat.timer) {
-          // Clear the heartbeat timer if it exists.
-          clearInterval(this.heartbeat.timer);
-        }
-
-        // Try to get the interval from sent data, fallback to default if 
-        // it's not provided (rare).      
-        if (data.heartbeat_interval) {
-          this.heartbeat.interval = data.heartbeat_interval;
-        }
-
-        debug('sending heartbeats with %oms interval',
-          this.heartbeat.interval);
-
-        // Send periodic heartbeats.
-        this.heartbeat.timer = setInterval(() => {
-          this.heartbeat.last = Date.now(); // To measure our ping.
-
-          // This will get cleared in the heartbeat ACK; if it doesn't, 
-          // we've exceeded our ping limit and should close the connection.
-          this.heartbeat.timeout = setTimeout(() => {
-            this.socket.close(4000, 'No heartbeat received.');
-          }, this.heartbeat.maxDelay);
-
-          debug('sending heartbeat');
-          this.send(this.payloads.heartbeat);
-        }, this.heartbeat.interval)
-
-        break;
-      // Heartbeat acknowledgement.
-      case 11:
-        clearInterval(this.heartbeat.timeout); // Clear the timeout catch.
-
-        // Calculate our ping.
-        this.connection.ping = Date.now() - this.heartbeat.last;
-
-        debug('heartbeat acknowledged with %oms delay', this.connection.ping);
-        break;
-      default: debug('unknown opcode: %o', message.op);
-    }
-
-    switch (message.t) {
-      case 'READY':
-        this.connection.sessionID = data.session_id;
-        break;
-      case 'MESSAGE_CREATE':
-        // Data should be a message object, see: 
-        // https://discordapp.com/developers/docs/resources/channel#message-object
-        this.emit('message', data);
-        break;
-    }
-  }
 }
+
+// Handle incoming websocket messages.
+Client.prototype.handle = require('./handle');
