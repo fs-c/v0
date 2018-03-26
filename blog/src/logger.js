ae2b01d5ad6e8e5cc8503d8866c38037e323b9ec
fsoc <8392143a@gmail.com>
2018-03-26 19:42:46 +0200

    Configure logger.

--- blog/src/logger.js
+++ blog/src/logger.js
@@ -1,13 +1,20 @@
-const { createLogger, transports, format } = require('winston');
+const {
+  createLogger, transports, format, addColors
+} = require('winston');
+
+const custom = format.printf((info) => {
+  return `${info.timestamp} - [${info.level}]: ${info.message}`;
+});
 
 const logger = exports.log = createLogger({
+  level: 'silly',
   format: format.combine(
     format.splat(),
     format.timestamp(),
-    format.simple()
+    custom
   ),
   transports: [
-    new transports.Console(),
+    new transports.Console({ level: 'silly' }),
     new transports.File({ filename: 'log/combined.log' }),
     new transports.File({ filename: 'log/error.log', level: 'error',  })    
   ]
