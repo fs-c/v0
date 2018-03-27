c70c3ba2b247002fbbf6c4fc1d99270ca2794834
lw <lw@thinkpad.home>
2018-03-27 22:41:59 +0200

    Basic server needs.

--- blog/src/server.js
+++ blog/src/server.js
@@ -1,7 +1,29 @@
-const Koa = require('koa');
+require('dotenv').config();
 
+const Koa = require('koa');
+const { join } = require('path');
 const { log } = require('./logger');
 
 const app = exports.app = new Koa();
 
-log.debug('bla');
+app.proxy = true;
+app.keys = [ process.env.SESSION ];
+
+const compress = require('koa-compress'); 
+app.use(compress());
+
+const serve = require('koa-static');
+app.use(serve(join(__dirname, 'public')));
+
+const render = require('koa-views');
+app.use(render(join(__dirname, 'views'), {
+  extension: 'ejs',
+}));
+
+app.use(async (ctx, next) => {
+  try { await next() } catch(err) {
+    ctx.render('status', { err });
+
+    log.warn(err.message);
+  }
+})
