97ce2f44ae9d8095a82729aabce6678d29bb00f6
lw <lw@thinkpad.home>
2018-03-28 14:09:08 +0200

    Remove testing response.

--- blog/src/router.js
+++ blog/src/router.js
@@ -1,6 +1,5 @@
 const Router = require('koa-router');
 const router = module.exports = new Router();
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('status', { message: 'yo.' });
-});
+router.get('/', async (ctx, next) => {});
+
