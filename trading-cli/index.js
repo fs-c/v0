0f016cd49e83572b55c160838d0ebd9b2c32cf19
lw2904 <8392143a@gmail.com>
2018-03-29 03:39:45 +0200

    Implement monitoring.

--- trading-cli/index.js
+++ trading-cli/index.js
@@ -53,10 +53,38 @@ async function listOffers() {
     console.log(`Offer ${offer.id} by ${sender} for ${recipient}.`);
     console.log(`   ${toGive} items to give, ${toReceive} items to receive.`);
     console.log(`   It was created ${created} and expires ${expires}.`);
+    console.log(`   Offer message: ${offer.message}.`);    
     console.log('')
   }
 }
 
-async function monitorEvents() {
+function offerChanged(offer, oldState) {
+  const olds = Trader.ETradeOfferState[oldState];
+  const news = Trader.ETradeOfferState[offer.state];
 
+  console.log(`offer ${offer.id} changed from ${olds} to ${news}`);
+}
+
+async function monitorEvents() {
+  await trader.initialise();
+
+  trader.on('ready', () => console.log('ready'));
+  
+  trader.on('newOffer', (offer) => {
+    console.log(
+      `new offer (${offer.id} - ${offer.state}) by ${offer.partner.toString()} `
+      + `with message: ${offer.message}.`
+    )
+  });
+
+  trader.on('sentOfferChanged', offerChanged);
+  trader.on('receivedOfferChanged', offerChanged);
+
+  trader.on('sentOfferCanceled', (offer) => {
+    console.log(`offer ${offer.id} has been canceled`)
+  });
+
+  trader.on('pollFailure', (err) => {
+    console.log(`failed polling: ${err.message}`);
+  });
 }
