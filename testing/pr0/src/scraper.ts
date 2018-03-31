203e7191d6c021f668698302a1fdbde4c535ea09
fsoc <8392143a@gmail.com>
2018-03-31 11:19:02 +0200

    Add getItems functionality.

--- testing/pr0/src/scraper.ts
+++ testing/pr0/src/scraper.ts
@@ -4,6 +4,8 @@ import * as minimist from 'minimist';
 import { Pr0grammAPI, ItemFlags } from 'pr0gramm-api';
 import { readFileSync, writeFileSync, existsSync } from 'fs';
 
+(async () => {
+
 const args = minimist(process.argv.slice(2));
 
 if (!args.s) { debug.enable('scraper'); }
@@ -14,9 +16,26 @@ const interval = (args.i || 30) * 1000;
 const log = debug('scraper');
 const api = Pr0grammAPI.createWithCookies();
 
-const { items } = JSON.parse(
-  readFileSync(join(path, 'items.json'), 'utf8')
-);
+const getItems = async () => {
+  if (existsSync(join(path, 'items.json')) && !args.r) {
+    log('reading items from disk');
+
+    return JSON.parse(readFileSync(join(path, 'items.json'), 'utf8'));
+  }
+
+  log('fetching items');
+
+  const items = await api.items.getItems({
+    promoted: true,
+    flags: ItemFlags.SFW,
+  });
+
+  writeFileSync(join(path, 'items.json'), JSON.stringify(items));
+
+  return items;
+}
+
+const { items } = await getItems();
 
 log('working through %o posts with an interval of %o',
   items.length, interval);
@@ -49,3 +68,5 @@ async function get(index: number) {
 
   setTimeout(get, interval, index + 1);
 }
+
+})();
