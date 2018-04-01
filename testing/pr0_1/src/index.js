ac72ef7bbeb46e6c96b6860c52f0dae85aece841
fsoc <8392143a@gmail.com>
2018-04-01 19:17:58 +0200

    Rework get function.

--- testing/pr0_1/src/index.js
+++ testing/pr0_1/src/index.js
@@ -1,23 +1,42 @@
 const { join } = require('path');
 const { log } = require('@sturmwalzer/logger');
-const { Pr0grammAPI, ItemFlags } = require('pr0gramm-api');
 const { writeFileSync, readFileSync } = require('fs');
+const { Pr0grammAPI, ItemFlags } = require('pr0gramm-api');
 
 const args = require('minimist')(process.argv.slice(2));
 const dataPath = args.data;
 
 const api = Pr0grammAPI.createWithCookies();
 
-async function get(id) {
-  log('getting items newer than %o', id);
+const capFirst = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-  const response = await api.items.getItemsNewer({
-    newer: id,
+async function get() {
+  const response = await api.items['getItems' + (
+    args.newer ? 'Newer' : 
+    args.older ? 'Older' : 
+    args.around ? 'Around' : ''
+  )]({
+    newer: args.newer,
+    older: args.older,
+    around: args.around,
+
     promoted: true,
     flags: ItemFlags.SFW,
   });
 
-  log(response)
+  if (response.error) {
+    log('something went wrong: %o', response.error);
+  }
+
+  const { items } = response;
+
+  log('got %o items', items.length);
+
+  writeFileSync('res.json', JSON.stringify(
+    items.sort((a, b) => a.ts - b.ts)
+  ));
 }
 
-get(args.start);
+get();
