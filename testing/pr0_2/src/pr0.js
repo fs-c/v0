54428adbfe2b80f6779a6e14187b97b8f80da785
fsoc <8392143a@gmail.com>
2018-04-01 21:25:32 +0200

    Fix request function, add some debug logging.

--- testing/pr0_2/src/pr0.js
+++ testing/pr0_2/src/pr0.js
@@ -1,18 +1,32 @@
 const { get } = require('got');
+const debug = require('debug')('api');
+const { URLSearchParams } = require('url');
 
 const base = 'http://pr0gramm.com/api/';
 
 /**
- * Send a request to the given route with the params parsed as queries.
+ * Send a request to the given route with the params 
+ * parsed as queries.
  * @param {string} route 
  * @param {object} params 
  * @returns {Promise}
  */
-const request = async (route, params = { promoted: 1, flags: 1 }) => {
-  return (await get(base + route, {
+const request = async (
+  route,
+  params = { promoted: 1, flags: 1 }
+) => {
+  const url = base + route + '?'
+    + new URLSearchParams(params).toString();
+  
+  debug('GET: %o', url);
+
+  const res = await get(url, {
     json: true,
-    query: params,
-  })).body;
+  });
+
+  debug('%o/%o', res.statusCode, res.statusMessage);
+
+  return res.body;
 };
 
 const api = exports.api = {
@@ -23,13 +37,13 @@ const api = exports.api = {
       return this.get(opts);
     },
     older(id, opts) {
-      return this.get(Object.merge({ older: id }, opts));
+      return this.get(Object.assign({ older: id }, opts));
     },
     newer(id, opts) {
-      return this.get(Object.merge({ newer: id }, opts));
+      return this.get(Object.assign({ newer: id }, opts));
     },
     around(id, opts) {
-      return this.get(Object.merge({ around: id }, opts));
+      return this.get(Object.assign({ around: id }, opts));
     },
 
     info(id) {
