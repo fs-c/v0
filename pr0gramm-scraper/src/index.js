b893425da0b20b7972f5e54ce5061b6883568933
fsoc <8392143a@gmail.com>
2018-04-03 15:02:48 +0200

    Initial.

---
+++ pr0gramm-scraper/src/index.js
@@ -0,0 +1,21 @@
+const { api } = require('./pr0');
+const { join } = require('path');
+// const Progress = require('progress');
+const { log } = require('@sturmwalzer/logger');
+const { writeFileSync, existsSync } = require('fs');
+
+const args = require('minimist')(process.argv.slice(2));
+
+const { data, lower } = args;
+const interval = args.interval || 30 * 1000;
+
+async function main() {
+  const initial = (
+    await api.items.newest({ promoted: 0 })
+  ).items;
+
+  const upper = initial[0].id;
+  log('getting posts beween %o (hi) and %o (lo)', upper, lower);
+
+  processBatch((await api.items.older(upper)).items)
+}
