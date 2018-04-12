5ad0d6d1665711a40202209b72322cd1157669f3
lw <me@lw.ag>
2018-04-12 12:02:48 +0200

    Refactor commands into functions.

--- rrl-chapter-cli/src/index.js
+++ rrl-chapter-cli/src/index.js
@@ -12,61 +12,63 @@ const api = new RoyalRoadAPI();
 
 program.command('parse <file>', 'Parse a markdown file into RRL readable HTML.')
   .option('--out <path>', 'File to write parsed markdown to.')
-  .action(async (file, opts) => {
-    const html = parseMarkdown(await getFile(file));
-
-    if (opts.out) {
-      writeFile(join(cwd(), file), html, (err) => {
-        if (err) {
-          console.error('Failed to write file: ' + err.message);
-        }
-      });
-    }
-
-    process.stdout.write(html);
-    process.stdout.end();
-
-    process.exit(1);
-  });
+  .action(parseFile);
 
 program.command('publish [file]')
   .option('-a, --ask', 'Prompt for required information.')
-  .action(async (file, opts) => {
-    const content = parseMarkdown(await getFile(file));
-
-    let title, fiction, username, password;
-
-    if (opts.ask) {
-      title = title || question('Chapter title: ');
-      username = username || question('Username: ');
-      password = password || password('Password: ');
-    }
-
-    try {
-      await api.user.login(username, password);
-      
-      const myFictions = await api.user.myFictions();
-      const fiction = myFictions[
-        keyInSelect(myFictions.map((fic) => fic.title))
-      ].id;
-
-      const res = await api.fiction.publishChapter(fiction, {
-        title,
-        content,
-        preNote: '',
-        postNote: '',
-      });
-
-      writeFile('res.html', res);
-    } catch (err) {
-      console.log('RRL API error: ' + err.message);
-    }
-
-    console.log('Published chapter.');
-  });
+  .action(publishChapter);
 
 program.version(require('../package.json').version);
 program.parse(process.argv);
+
+async function parseFile(file, opts) {
+  const html = parseMarkdown(await getFile(file));
+
+  if (opts.out) {
+    writeFile(join(cwd(), file), html, (err) => {
+      if (err) {
+        console.error('Failed to write file: ' + err.message);
+      }
+    });
+  }
+
+  process.stdout.write(html);
+  process.stdout.end();
+
+  process.exit(1);
+}
+
+async function publishChapter(file, opts) {
+  const content = parseMarkdown(await getFile(file));
+
+  let title, fiction, username, password;
+
+  if (opts.ask) {
+    title = title || question('Chapter title: ');
+    username = username || question('Username: ');
+    password = password || password('Password: ');
+  }
+
+  try {
+    await api.user.login(username, password);
+    
+    const myFictions = await api.user.myFictions();
+    const fiction = myFictions[
+      keyInSelect(myFictions.map((fic) => fic.title))
+    ].id;
+
+    const res = await api.fiction.publishChapter(fiction, {
+      title,
+      content,
+      preNote: '',
+      postNote: '',
+    });
+  } catch (err) {
+    console.log('RRL API error: ' + err.message);
+  }
+
+  console.log('Published chapter.');
+}
 
 /**
  * Parses markdown into HTML that is readable by RRL.
