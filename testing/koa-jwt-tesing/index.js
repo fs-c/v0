ff4972a3d49c14636c500d9e6d3433165721d76c
lw <me@lw.ag>
2018-04-23 19:02:07 +0200

    Do some testing.

--- testing/koa-jwt-tesing/index.js
+++ testing/koa-jwt-tesing/index.js
@@ -1,22 +1,52 @@
-const _ = require('koa-route');
-const app = new require('koa')();
-const token = require('jsonwebtoken');
-const jwt = require('koa-jwt')({ secret: String(Date.now()) });
+const secret = Date.now();
 
-async function authenticate(data) {
+const _ = require('koa-route');
+const app = new (require('koa'))();
+const token = require('jsonwebtoken');
+
+app.use(require('koa-logger')());
+
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    if (err.status === 401) {
+      ctx.body = `
+        <form method="POST" action="/">
+          <input name="name">
+          <input name="password" type="password">
+          <button type="submit">Submit</button>
+        </form>
+      `;
+    }
+  }
+});
+
+app.use(_.post('/', async (ctx, next) => {
+  console.log('reached the / post function');
+
+  const { body } = ctx.request.body;
+
+  if (data.password !== 'pass') {
+    return ctx.throw(401);
+  }
+
   ctx.body = {
-    token: token.sign({ name: data.name }, )
+    token: token.sign({
+      data: body,
+      exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    }, secret),    
   };
-}
+
+  ctx.redirect('/');
+
+  await next();
+}));
+
+app.use(require('koa-jwt')({ secret }));
 
 app.use(_.get('/', async (ctx) => {
-  ctx.body = 'API root.';
+  ctx.body = 'Protected.';
 }));
 
-app.use(_.get('/protected', jwt, async (ctx) => {
-  ctx.body = 'Protected enpoint.';
-}));
-
-app.use(_.post(ctx('/authenticate', async (ctx) => {
-  authenticate(ctx.body);
-})));
+app.listen(8080);
