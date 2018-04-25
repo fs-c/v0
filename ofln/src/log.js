70f251e6f8caf4d576d8e55e8b44efe8fe40980c
lw <me@lw.ag>
2018-04-26 01:44:24 +0200

    Add simple logger.

---
+++ ofln/src/log.js
@@ -0,0 +1,33 @@
+const { inspect } = require('util');
+
+const write = (level, string) => {
+  process.stdout.write(`${level}: ${string}\n`);
+};
+
+const inspectArgs = {
+  depth: null,
+  colors: true,
+  compact: true,
+};
+
+const log = (level, body, ...args) => {
+  if (typeof body !== 'string') {
+    return write(level, inspect(body));
+  }
+
+  let index = -1;
+  const string = body.replace(/%o/g, () => {
+    index++;
+
+    return inspect(args[index], inspectArgs);
+  });
+
+  write(level, string);
+};
+
+exports.default = exports.log = log;
+
+const levels = [ 'warn', 'info', 'debug' ];
+for (const level of levels) {
+  exports[level] = (...args) => log(level, ...args);
+}
