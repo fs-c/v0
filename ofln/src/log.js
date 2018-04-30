4606a8ad87e2b7d8095a2af077c0ac5c9edf863e
lw <me@lw.ag>
2018-04-30 14:44:36 +0200

    Initial.

---
+++ ofln/src/log.js
@@ -0,0 +1,39 @@
+const { inspect } = require('util');
+
+const active = process.env.LEVEL || 'info';
+
+const write = (level, string) => {
+  process.stdout.write(`${level}: ${string}\n`);
+};
+
+const inspectArgs = {
+  depth: null,
+  colors: true,
+  compact: true,
+};
+
+const log = (level, body, ...args) => {
+  if (typeof body !== 'string') {
+    return write(level, inspect(body));
+  }
+
+  let index = -1;
+  const string = body.replace(/%o/g, () => {
+    index++;
+
+    return inspect(args[index], inspectArgs);
+  });
+
+  write(level, string);
+};
+
+exports.default = exports.log = log;
+
+const levels = [ 'error', 'warn', 'info', 'debug' ];
+for (const index in levels) {
+  const level = levels[index];
+
+  if (levels.indexOf(active) <= index) {
+    exports[level] = (...args) => log(level, ...args);
+  }
+}
