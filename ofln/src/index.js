990e2c2169ab4fdd4940d7b6460e22e4d74ec2c3
lw <me@lw.ag>
2018-04-30 14:47:21 +0200

    Move node logic to own file.

--- ofln/src/index.js
+++ ofln/src/index.js
@@ -1,6 +1,6 @@
 const { URL } = require('url');
-const { parse } = require('parse5');
 const { get } = require('./request');
+const { getNodes } = require('./parse');
 const { error, debug } = require('./log');
 
 const args = process.argv.splice(2).reduce((acc, cur, i, arr) => {
@@ -10,35 +10,6 @@ const args = process.argv.splice(2).reduce((acc, cur, i, arr) => {
 }, {  });
 
 const target = new URL(args['--target']);
-
-const getNode = (parent, name) => getNodes(parent, name, 1)[0];
-const getNodes = (parent, name, max) => {
-  let nodes = [];
-
-  const traverseNodes = (node) => {
-    const children = node.childNodes;
-
-    if (children) {
-      debug('traversing %o (%o children)', parent.nodeName, children.length)
-
-      for (const child of children) {
-        if (!name || (name && child.nodeName === name)) {
-          debug('found node %o', child.nodeName);
-
-          nodes.push(child);
-        }
-
-        if (!max || (max && max > nodes.length)) {
-          traverseNodes(child);
-        }
-      }
-    }
-  }
-
-  traverseNodes(parent);
-
-  return nodes;
-};
 
 const processPage = async (html) => {
   const document = parse(html);
---
+++ ofln/src/parse.js
@@ -0,0 +1,33 @@
+const { parse } = require('parse5');
+
+const getNode = (parent, name) => getNodes(parent, name, 1)[0];
+const getNodes = (parent, name, max) => {
+  let nodes = [];
+
+  const traverseNodes = (node) => {
+    const children = node.childNodes;
+
+    if (children) {
+      debug('traversing %o (%o children)', parent.nodeName, children.length)
+
+      for (const child of children) {
+        if (!name || (name && child.nodeName === name)) {
+          debug('found node %o', child.nodeName);
+
+          nodes.push(child);
+        }
+
+        if (!max || (max && max > nodes.length)) {
+          traverseNodes(child);
+        }
+      }
+    }
+  }
+
+  traverseNodes(parent);
+
+  return nodes;
+};
+
+exports.getNode = getNode;
+exports.default = exports.getNodes = getNodes;
