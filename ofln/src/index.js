3ae3b6d6a1628c2f843ea9fab89c24cddb50de3d
lw <me@lw.ag>
2018-05-02 16:34:22 +0200

    Add help message.

--- ofln/src/index.js
+++ ofln/src/index.js
@@ -11,6 +11,19 @@ const args = process.argv.splice(2).reduce((acc, cur, i, arr) => {
   return acc;
 }, {  });
 
+if (args['--help']) {
+  console.log(
+    '\nUsage: ofln [OPTIONS]\n' +
+    '\nOptions:\n' +
+    '  --help     Show this help message.\n' +
+    '  --out      Relative path to the desired output dir.'
+      + ' Will be created if needed.\n' +
+    '  --target   Full link to the page to be saved.'
+  );
+
+  process.exit(1);
+}
+
 // The absolute path to the output directory.
 const out = path.resolve(args['--out']);
 // A URL object of the page to save.
