b1cb6c0d1c4c893ff4c24628fb8f26c705a91e6e
lw <me@lw.ag>
2018-06-01 12:40:04 +0200

    Refactor getFriends and getCountry.

--- steam-countries/src/steam.js
+++ steam-countries/src/steam.js
@@ -1,34 +1,44 @@
 const got = require('got');
 const cheerio = require('cheerio');
 const log = require('./logger')('steam');
-const { URLSearchParams } = require('url');
 
+const API_KEY = process.env.STEAM_API_KEY;
+require('assert')(API_KEY);
+
+/**
+ * Get the friend list for a given Steam ID using the Steam Web API.
+ * 
+ * @param {number} base - Steam64ID
+ */
 const getFriends = async (base) => {
-    const path = 'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?';
-    const params = new URLSearchParams({
-        steamid: base,
-        relationship: 'all',
-        key: process.env.STEAM_API_KEY,
+    const path = 'http://api.steampowered.com/ISteamUser/GetFriendList/v0001';
+    const { body } = await got(path, {
+        json: true,
+        query: {
+            key: API_KEY,
+            relationship: 'all',
+            steamid: base.toString(),
+        },
     });
 
-    log.trace({ from: base }, 'getting friend list')
-
-    const { body } = await got(path + params.toString(), { json: true });
-
-    const friends = body.friendslist.friends;
-
-    log.trace({ friends: friends.length, from: base }, 'got friend list');
+    const friends = (body.friendslist || {}).friends || [];
+    log.trace({ base, friends: friends.length }, 'got friend list');
 
     return friends.map((e) => e.steamid);
 };
 
+/**
+ * Get the country code a Steam ID is associated with.
+ * 
+ * @param {number} id - Steam64ID
+ */
 const getCountry = async (id) => {
-    const uri = 'https://steamcommunity.com/profiles/' + id;
-    const { body } = await got(uri);
+    const { body } = await got('https://steamcommunity.com/profiles/' + id);
+
+    log.trace({ id }, 'got profile');    
+
     const $ = cheerio.load(body);
     const flag = $('img.profile_flag');
-
-    log.trace({ id }, 'got profile');
 
     if (flag && flag.attr('src')) {
         const code = flag.attr('src').split('/').reverse()[0].split('.')[0];
@@ -39,7 +49,7 @@ const getCountry = async (id) => {
     } else {
         log.trace('no country set');
 
-        return 'zz';
+        return null;
     }
 };
 
