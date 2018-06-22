481778a998ab9629f850b6e3abf8699e6ed8f505
lw <me@lw.ag>
2018-06-22 15:00:14 +0200

    Implement multithreading.

--- testing/osu-node/getstart.js
+++ testing/osu-node/getstart.js
@@ -1,24 +1,39 @@
-const chalk = require('chalk');
 const robot = require('robotjs');
-const iohook = require('iohook');
+const cluster = require('cluster');
 
 const points = [
-    { x: 200, y: 950, cur: 0, las: 0, },
-    { x: 230, y: 950, cur: 0, las: 0, },
-    { x: 260, y: 950, cur: 0, las: 0, },
-    { x: 290, y: 950, cur: 0, las: 0, },
-]
+    { x: 200, y: 950, },
+    { x: 230, y: 950, },
+    { x: 260, y: 950, },
+    { x: 290, y: 950, },
+];
 
-let i = 0;
-setInterval(() => {
-    console.time(i);
-    for (let i = 0; i < points.length; i++) {
-        const hx = robot.getPixelColor(points[i].x, points[i].y);
+const scan = (point) => {
+    let last = 0;
 
-        const dif = hx - points[i].las;
-        if (dif) console.log(points[i].x, hx, di);
+    setInterval(() => {
+        console.time(`${process.pid}`);
 
-        points[i].las = hx;
+        const hx = robot.getPixelColor(point.x, point.y);
+
+        const dif = hx - last;
+        if (dif)
+            console.log(point.x, hx, dif);
+
+        last = hx;
+
+        console.timeEnd(`${process.pid}`);
+    }, 1);
+};
+
+if (cluster.isMaster) {
+    for (const point of points) {
+        const worker = cluster.fork({ point: JSON.stringify(point) });
+
+        worker.send(JSON.stringify(point));
     }
-    console.timeEnd(i++ - 1);    
-}, 1);
+} else {
+    try {
+        scan(JSON.parse(process.env.point));
+    } catch (err) { console.error(err); }
+}
