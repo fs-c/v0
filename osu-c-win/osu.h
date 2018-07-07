87a67fde4f189811a530a13bec28c466b649dbf9
lw <me@lw.ag>
2018-07-08 01:23:04 +0200

    Add comments to function declarations.

--- osu-c-win/osu.h
+++ osu-c-win/osu.h
@@ -1,10 +1,19 @@
 #include <windows.h>
 
-#define TAPTIME_MS 2
 #define TIME_ADDRESS 0x052F59EC
 
 extern HANDLE game_proc;
 
-/* proc.c */
+/**
+ * proc.c
+ * Returns the length the current maps song has been running for (aka the
+ * gametime) from the game process.
+ */
 INT32 get_gametime();
+
+/**
+ * proc.c
+ * Returns the ID of the process with the given name. Returns zero if the
+ * process was not found or fetching the processes failed. Case sensitive.
+ */
 DWORD get_process_id(char *name);
