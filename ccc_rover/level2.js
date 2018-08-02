308ff6fe8550194b7e25ba0c8fe3e6940392273f
lw <me@lw.ag>
2018-08-02 23:48:36 +0200

    Add debug logging.

--- ccc_rover/level2.js
+++ ccc_rover/level2.js
@@ -9,7 +9,7 @@ const strings = [
 	"2.70 45.00 -34.00",
 	"4.20 -5.30 20.00",
 	"9.53 8.12 0.00",
-]
+];
 
 const degToRad = (deg) => deg * (Math.PI / 180);
 const radToDeg = (rad) => rad * (180 / Math.PI);
@@ -18,10 +18,12 @@ const solve = (wheelBase, distance, steeringAngle) => {
 	const radius = wheelBase / Math.sin(degToRad(steeringAngle));
 	const angle = radToDeg(distance / radius);
 
-	const x = ((radius * -1) + (radius * Math.cos(degToRad(angle)))) * -1;
+	const x = radius * Math.cos(degToRad(angle)) * -1 + radius;
 	const y = radius * Math.sin(degToRad(angle));
 
-	console.log(`${x} ${y} ${angle}`);
-}
+	console.log(`[ r=${radius}, a=${angle} ]: ${x}/${y}`);
+
+	console.log(`${x} ${y} ${angle}\n`);
+};
 
 strings.map((e) => solve(...(e.split(' ').map((f) => parseFloat(f)))));
