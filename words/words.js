532b6b6bcebf0501134f977d8b7bbf998b1d9111
lw <me@lw.ag>
2018-08-30 23:28:26 +0200

    Implement template filling and writing

--- words/words.js
+++ words/words.js
@@ -18,8 +18,9 @@
 const fs = require('fs');
 const path = require('path');
 const marked = require('marked');
+const { highlight } = require('highlightjs');
 
-const { parseItem } = require('./src/utils');
+const { parseItem, copyFolder, removeFolder } = require('./src/utils');
 
 const cwd = require('process').cwd();
 
@@ -28,16 +29,40 @@ const items = fs.readdirSync(path.join(cwd, 'content'))
     .map((item) => path.resolve('content', item), 'utf8').map(parseItem)
     .filter((item) => item);
 
+// Absolute path to the public/ folder.
 const publicPath = path.resolve(path.join(cwd, 'public'));
+
+// Remove and recreate it if it exists.
 if (fs.existsSync(publicPath))
     removeFolder(publicPath);
 
 fs.mkdirSync(publicPath);
 
+// Copy static files from static/ into public/static/.
 copyFolder(path.resolve(path.join(cwd, 'static')),
     path.resolve(path.join(cwd, 'public/static')));
 
+const templates = {
+    item: fs.readFileSync('templates/item.html', 'utf8'),
+}
+
+marked.setOptions({
+    highlight: (code, lang) => highlight(lang, code).value,
+});
+
 for (const item of items) {
+    // Absolute path to the folder for the item in public/.
     const itemPath = path.join(publicPath, path.parse(item.path).name);
-    fs.mkdirSync(path.join(itemPath));
+    fs.mkdirSync(itemPath);
+
+    const data = {
+        content: marked(item.content),
+        title: item.frontMatter.title,
+    };
+
+    const template = Object.keys(data).reduce((acc, cur, i) => {
+        return acc.replace(`<%${cur.toUpperCase()}%>`, data[cur]);
+    }, templates.item);
+
+    fs.writeFileSync(path.join(itemPath, 'index.html'), template);
 }
