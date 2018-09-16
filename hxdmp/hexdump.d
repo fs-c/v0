584fc5a69f4a07a288bb4cdc483513102f635be1
lw <me@lw.ag>
2018-09-16 19:41:34 +0200

    Improve column space placement

--- hxdmp/hexdump.d
+++ hxdmp/hexdump.d
@@ -10,37 +10,32 @@ int main()
 
 	foreach (ubyte[] buffer; stdin.byChunk(chunk_size)) {
 		writef("%08x: ", i);
-
 		i += chunk_size;
 
-		handle_chunk(buffer, col_size);
-
-		// writefln(buffer[0]);
+		handle_chunk(buffer, chunk_size, col_size);
 	}
 
 	return 0;
 }
 
-void handle_chunk(const ubyte[] chunk, const int col_size)
+void handle_chunk(const ubyte[] chunk, const int chunk_size, const int col_size)
 {
-	int col_i;
-	string s;
+	string text;
 
-	foreach (b; chunk) {
+	foreach (i, b; chunk) {
 		char c = cast(char)b;
 
 		if (c == '\n' || c == '\t' || c == ' ')
 			c = '.';
-		
-		s ~= c;
+
+		text ~= c;
 
 		writef("%02x", b);
 
-		if (++col_i == col_size) {
-			col_i = 0;
+		if (!((i + 1) % col_size)) {
 			write(" ");			
 		}
 	}
 
-	writef(" %s\n", s);
+	writef(" %s\n", text);
 }
