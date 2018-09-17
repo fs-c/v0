2d8a4fd98f31b91ffae00c9a1685bd2a057b119b
lw <me@lw.ag>
2018-09-17 16:34:12 +0200

    Add TODO comment and improve formatting

--- hxdmp/hexdump.d
+++ hxdmp/hexdump.d
@@ -10,8 +10,7 @@ int main()
 	immutable chunk_size = cols * col_size;
 
 	foreach (ubyte[] buffer; stdin.byChunk(chunk_size)) {
-		writef("%08x: ", i);
-		i += chunk_size;
+		writef("%08x: ", i++ * chunk_size);
 
 		handle_chunk(buffer, chunk_size, col_size);
 	}
@@ -30,18 +29,20 @@ void handle_chunk(const ubyte[] chunk, const int chunk_size, const int col_size)
 			ubyte b = chunk[i];
 			char c = cast(char)b;
 
+			// TODO: Implement a more general approach to parsing
+			// 	 out control sequences
 			if (c == '\n' || c == '\t' || c == ' ')
 				c = '.';
-			
-			text ~= c;			
-			
+
+			text ~= c;
+
 			writef("%02x", b);
 		// If it is, pad out the remaining space
 		} else {
 			write("  ");
 		}
 
-		// Prefer this over modulo for performance.
+		// Prefer this over modulo for performance
 		if (++col_i >= col_size) {
 			col_i = 0;
 
