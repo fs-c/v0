a665249623805896f231c6db4dcb9624c6312000
lw <me@lw.ag>
2018-09-24 09:31:37 +0200

    Initial

---
+++ pascal_sort/unit1.pas
@@ -0,0 +1,55 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Grids,
+  StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    StringGrid1: TStringGrid;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  Numbers: array [0..20000] of Integer;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+     // StringGrid1.RowCount := length(Numbers);
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  i: Integer;
+begin
+     randomize();
+
+     for i := 0 to length(Numbers) - 1 do begin
+         Numbers[i] := random(100000);
+
+         StringGrid1.cells[i mod 15, i div 15] := IntToStr(Numbers[i]);
+     end;
+end;
+
+end.
+
