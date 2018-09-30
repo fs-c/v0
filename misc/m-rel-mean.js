b75e1ab63741b2ec5be5543361dda415343ade68
lw <me@lw.ag>
2018-09-30 20:33:37 +0200

    Add benchmark

--- misc/m-rel-mean.js
+++ misc/m-rel-mean.js
@@ -1,7 +1,9 @@
-let total = 0;
-const runs = process.argv[2];
+let runs = process.argv[2], total = 0;
+
+console.time('ran in');
 
 for (let i = 0; i < runs; i++)
     total += Math.round(Math.random());
 
 console.log(total / runs);
+console.timeEnd('ran in');
