136afc9829707384462711b2d7f31e7e1cf15a6d
lw <me@lw.ag>
2018-10-01 13:16:21 +0200

    Add comments and improve readability

--- pascal_sort/unit1.pas
+++ pascal_sort/unit1.pas
@@ -18,9 +18,8 @@ type
     StringGrid1: TStringGrid;
     procedure ButtonGenerateClick(Sender: TObject);
     procedure ButtonBubbleSortClick(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
 
-    procedure WriteToTable(Arr: array of Integer);
+    procedure WriteToGrid(Arr: array of Integer; Grid: TStringGrid);
   private
 
   public
@@ -37,51 +36,56 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.FormCreate(Sender: TObject);
-begin
-     // StringGrid1.RowCount := length(Numbers);
-end;
-
 procedure TForm1.ButtonGenerateClick(Sender: TObject);
 var
   i: Integer;
 begin
-     randomize();
+     Randomize;
 
-     for i := 0 to length(Numbers) - 1 do begin
+     for i := 0 to Length(Numbers) - 1 do begin
+         { Fill with Integers from [0, 100000). }
          Numbers[i] := random(100000);
      end;
 
-     WriteToTable(Numbers);
+     WriteToGrid(Numbers, StringGrid1);
 end;
 
 procedure TForm1.ButtonBubbleSortClick(Sender: TObject);
 var
-  i, j, t: Integer;
+  outer_i, inner_i, tmp: Integer;
 begin
-     for i := 0 to length(Numbers) - 1 do begin
-         t := 0;
-         for j := 0 to length(Numbers) - 1 do begin
-             if Numbers[j] > Numbers[j + 1] then begin
-                t := Numbers[j];
-                Numbers[j] := Numbers[j + 1];
-                Numbers[j + 1] := t;
-             end;
-         end;
+     for outer_i := 0 to Length(Numbers) - 1 do begin
+         { Set to zero to be able to detect bailout condition. }
+         tmp := 0;
 
-         if t = 0 then
+         for inner_i := 0 to Length(Numbers) - 1 do
+             if Numbers[inner_i] > Numbers[inner_i + 1] then begin
+                { Swap values. }
+                tmp := Numbers[inner_i];
+                Numbers[inner_i] := Numbers[inner_i + 1];
+                Numbers[inner_i + 1] := tmp;
+             end;
+
+         { If we never swapped values in this run, break out. }
+         if tmp = 0 then
             break;
      end;
 
-     WriteToTable(Numbers);
+     WriteToGrid(Numbers, StringGrid1);
 end;
 
-procedure TForm1.WriteToTable(Arr: array of Integer);
+{ Writes an array of Integers of variable size to a given StringGrid. Fails if
+  the array does not fit into the grid. Moves horizontal first, from left to
+  right and top to bottom. }
+procedure TForm1.WriteToGrid(Arr: array of Integer; Grid: TStringGrid);
 var
   i: Integer;
 begin
+     if Grid.ColCount * Grid.RowCount < Length(Arr) then
+        Exit;
+
      for i := 0 to length(Arr) - 1 do begin
-         StringGrid1.cells[i mod 15, i div 15] := IntToStr(Arr[i]);
+         Grid.cells[i mod 15, i div 15] := IntToStr(Arr[i]);
      end;
 end;
 
