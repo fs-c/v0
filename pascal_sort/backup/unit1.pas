5f1c777b128a821d25c8c51b646fe44611b84285
lw <me@lw.ag>
2018-10-10 23:40:29 +0200

    Implement insertion sort

--- pascal_sort/backup/unit1.pas
+++ pascal_sort/backup/unit1.pas
@@ -6,18 +6,22 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Grids,
-  StdCtrls;
+  StdCtrls, DateUtils;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    ButtonInsertionSort: TButton;
     ButtonGenerate: TButton;
     ButtonBubbleSort: TButton;
+    LabelIterations: TLabel;
+    LabelTime: TLabel;
     StringGrid1: TStringGrid;
     procedure ButtonGenerateClick(Sender: TObject);
     procedure ButtonBubbleSortClick(Sender: TObject);
+    procedure ButtonInsertionSortClick(Sender: TObject);
 
     procedure WriteToGrid(Arr: array of Integer; Grid: TStringGrid);
   private
@@ -52,19 +56,26 @@ end;
 
 procedure TForm1.ButtonBubbleSortClick(Sender: TObject);
 var
-  outer_i, inner_i, tmp: Integer;
+  time_start: TDateTime;
+  time_elapsed, iterations, outer_i, inner_i, tmp: Integer;
 begin
+     iterations := 0;
+     time_start := Now;
+
      for outer_i := 0 to Length(Numbers) - 1 do begin
          { Set to zero to be able to detect bailout condition. }
          tmp := 0;
 
-         for inner_i := 0 to Length(Numbers) - 1 do
+         for inner_i := 0 to Length(Numbers) - 1 do begin
              if Numbers[inner_i] > Numbers[inner_i + 1] then begin
                 { Swap values. }
                 tmp := Numbers[inner_i];
                 Numbers[inner_i] := Numbers[inner_i + 1];
                 Numbers[inner_i + 1] := tmp;
              end;
+         end;
+
+         iterations := iterations + ++inner_i;
 
          { If we never swapped values in this run, break out. }
          if tmp = 0 then
@@ -72,12 +83,64 @@ begin
      end;
 
      WriteToGrid(Numbers, StringGrid1);
+
+     time_elapsed := MillisecondsBetween(time_start, Now);
+
+     LabelIterations.Caption := IntToStr(iterations);
+     LabelTime.Caption := IntToStr(time_elapsed) + 'ms';
+end;
+
+procedure TForm1.ButtonInsertionSortClick(Sender: TObject);
+var
+  time_start: TDateTime;
+  i, j, tmp, iterations: Integer;
+begin
+     i := 0;
+     j := 0;
+     iterations := 0;
+     time_start := Now;
+
+     {
+     while i < Length(Numbers) do begin
+         Inc(i);
+         j := i;
+
+         while (j > 0) and (Numbers[j - 1] > Numbers[j]) do begin
+               tmp := Numbers[j];
+               Numbers[j] := Numbers[j - 1];
+               Numbers[j - 1] := tmp;
+
+               Dec(j);
+         end;
+     end;
+     }
+
+     for i := 0 to Length(Numbers) do begin
+         j := i;
+         tmp := Numbers[i];
+
+         while (j > 0) and (Numbers[j - 1] > tmp) do begin
+             Numbers[j] := Numbers[j - 1];
+
+             Dec(j);
+         end;
+
+         iterations := iterations + i - 1 - j;
+
+         Numbers[j] := tmp;
+     end;
+
+     WriteToGrid(Numbers, StringGrid1);
+
+     LabelIterations.Caption := IntToStr(iterations);
+     LabelTime.Caption := IntToStr(MillisecondsBetween(time_start, Now))
+                       + 'ms';
 end;
 
 { Writes an array of Integers of variable size to a given StringGrid. Fails if
   the array does not fit into the grid. Moves horizontal first, from left to
   right and top to bottom. }
-procedure TForm1.(WriteToGridArr: array of Integer; Grid: TStringGrid);
+procedure TForm1.WriteToGrid(Arr: array of Integer; Grid: TStringGrid);
 var
   i: Integer;
 begin
