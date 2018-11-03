f0b5e1e4cf90056b13c376719fcd3e78593fc077
lw <me@lw.ag>
2018-11-03 18:46:04 +0100

    Fix mouse position manipulation

--- rythmic/src/mouse.c
+++ rythmic/src/mouse.c
@@ -2,5 +2,20 @@
 
 void set_mouse_position(int x, int y)
 {
-	SendMessage(game_window_handle, WM_MOUSEMOVE, 0, MAKELPARAM(x, y));
+	INPUT input;
+
+	input.type = INPUT_MOUSE;
+	
+	input.mi.dx = x;
+	input.mi.dy = y;
+	input.mi.mouseData = 0;
+	input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+	input.mi.time = 0;
+	input.mi.dwExtraInfo = 0;
+
+	if (!(SendInput(1, &input, sizeof(INPUT)))) {
+		debug("failed sending input to %d/%d", x, y);
+
+		return;
+	}
 }
