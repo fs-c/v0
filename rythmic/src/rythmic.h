7c8ca7068d0ee597015548f90945862cd10379f8
lw <me@lw.ag>
2018-11-03 18:46:18 +0100

    Reformat function macros

--- rythmic/src/rythmic.h
+++ rythmic/src/rythmic.h
@@ -18,18 +18,21 @@
 
 #ifdef DEBUG
 
+/* printf with added file and function name */
 #define debug(...)                                      \
 	printf("[debug] [%s:%s] ", __FILE__, __func__); \
 	printf(__VA_ARGS__);                            \
 	putchar('\n');					\
 
-#define debug_winerror(message) 						   \
-	wchar_t buf[256];							   \
-	FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, \
-		NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),   \
-		buf, (sizeof(buf) / sizeof(wchar_t)), NULL);			   \
-	printf("[debug] [%s:%s] [winerror] %s: %ls", __FILE__, __func__,	   \
-		message, buf);							   \
+/* Prints formatted GetLastError alongside a message */
+#define debug_winerror(message) 					\
+	wchar_t buf[256];						\
+	FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM 			\
+		| FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(),	\
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),		\
+		buf, (sizeof(buf) / sizeof(wchar_t)), NULL);		\
+	printf("[debug] [%s:%s] [winerror] %s: %ls", __FILE__, __func__,\
+		message, buf);						\
 
 #else
 
