343e4baf95c1ed6f2031bf6f98ec717082f2c44b
lw <me@lw.ag>
2018-11-04 20:49:20 +0100

    Play around with gametime fetching

--- rythmic/src/main.c
+++ rythmic/src/main.c
@@ -1,7 +1,8 @@
 #include "rythmic.h"
 
 pid_t game_proc_id = 0;
-RECT game_window_rect = {0};
+RECT game_window_rect = { 0 };
+void *game_time_address = NULL;
 HWND game_window_handle = NULL;
 HANDLE game_proc_handle = NULL;
 
@@ -25,6 +26,29 @@ int main()
 
 	if (!(get_window_coordinates(game_window_handle, &game_window_rect))) {
 		fatal("couldn't get window coordinates");
+	}
+
+	if (!(game_time_address = get_game_time_address())) {
+		fatal("couldn't get time address");
+	}
+
+	void *a = 0x13260c4;
+
+	debug("%d, %d", a == game_time_address, (int)a - (int)game_time_address);
+
+	debug("%#x -- %#x", game_time_address, a);
+
+	int32_t val1 = 0;
+	int32_t val2 = 0;
+
+	while (1) {
+		read_game_memory(a, &val1, 4, NULL);
+
+		get_game_time(&val2);
+
+		debug("%d / %d", val1, val2);
+
+		Sleep(200);
 	}
 
 	return 0;
