ef14a0157c9197f4814e2a5fc3e413584d957860
lw <me@lw.ag>
2018-11-05 18:34:07 +0100

    Make debug functions interchangeable

--- rythmic/src/rythmic.h
+++ rythmic/src/rythmic.h
@@ -25,14 +25,14 @@
 	putchar('\n');					\
 
 /* Prints formatted GetLastError alongside a message */
-#define debug_winerror(message) 					\
-	wchar_t buf[256];						\
-	FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM 			\
+#define debug_winerror(...) 						\
+	debug(__VA_ARGS__);						\
+	char wemsg[256];						\
+	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM 			\
 		| FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(),	\
 		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),		\
-		buf, (sizeof(buf) / sizeof(wchar_t)), NULL);		\
-	printf("[debug] [%s:%s] [winerror] %s: %ls", __FILE__, __func__,\
-		message, buf);						\
+		wemsg, (sizeof(wemsg) / sizeof(char)), NULL);		\
+	printf("[debug] [winerror] %s", wemsg);				\
 
 #else
 
