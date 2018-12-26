be9643abc396985ffa80b948bbb50e0d631c6814
lw <me@lw.ag>
2018-12-26 11:20:24 +0100

    Fix .osr reading up to byte stream

--- mimic/c/mimic.c
+++ mimic/c/mimic.c
@@ -8,11 +8,16 @@
 
 typedef unsigned char BYTE;
 
+void u16_clendian(uint16_t *x);
+void u32_clendian(uint32_t *x);
+
 uint64_t read_uleb128();
 size_t read_osu_string(char *out_buffer);
 
 FILE *stream;
 size_t rd = 0;
+
+int little_endian = 0;
 
 int main(int argc, char *argv[])
 {
@@ -30,32 +35,61 @@ int main(int argc, char *argv[])
 		return 1;
 	} else printf("opened file %s for reading\n", argv[1]);
 
+	int test_endian = 1;
+	if (*(char *)&test_endian == 1) {
+		printf("on a little-endian system\n");
+
+		little_endian = 1;
+	} else printf("on a big-endian system\n");
+
+	/* ruleset */
 	BYTE mode;
 	sread(&mode, sizeof(BYTE), 1);
 	printf("game mode: %d\n", (int)mode);
 
+	/* game version */
 	int32_t version;
 	sread(&version, sizeof(version), 1);
 	printf("game version: %d\n", version);
 
 	char string_buffer[2048];
 
+	/* md5 hash of beatmap */
 	read_osu_string(string_buffer);
-	printf("beatmap checksum: %s\n", string_buffer);
+	printf("beatmap hash: '%s'\n", string_buffer);
 
+	/* player name */
 	read_osu_string(string_buffer);
-	printf("player name: %s\n", string_buffer);
+	printf("player name: '%s'\n", string_buffer);
 	
+	/* md5 hash of the replay */
 	read_osu_string(string_buffer);
-	printf("replay checksum: %s\n", string_buffer);
+	printf("replay hash: '%s'\n", string_buffer);
 
-	sread(string_buffer, sizeof(uint16_t), 6);
+	uint16_t count = 0;
+
+	sread(&count, sizeof(uint16_t), 1);
+	printf("count300: %d\n", (int)count);
+	sread(&count, sizeof(uint16_t), 1);
+	printf("count100: %d\n", (int)count);
+	sread(&count, sizeof(uint16_t), 1);
+	printf("count50: %d\n", (int)count);
+	sread(&count, sizeof(uint16_t), 1);
+	printf("countGeki: %d\n", (int)count);
+	sread(&count, sizeof(uint16_t), 1);
+	printf("countKatu: %d\n", (int)count);
+	sread(&count, sizeof(uint16_t), 1);
+	printf("countMiss: %d\n", (int)count);
 
 	int32_t score;
 	sread(&score, sizeof(score), 1);
 	printf("score: %d\n", score);
 
-	int16_t perfect;
+	int16_t max_combo;
+	sread(&max_combo, sizeof(max_combo), 1);
+	printf("max_combo: %d\n", max_combo);
+
+	char perfect;
 	sread(&perfect, sizeof(perfect), 1);
 	printf("perfect: %d\n", perfect);
 
@@ -64,11 +98,15 @@ int main(int argc, char *argv[])
 	printf("mods used: %d\n", mods_used);
 
 	read_osu_string(string_buffer);
+	printf("life bar: '%s'\n", string_buffer);
 
-	int64_t timestamp;
+	int64_t timestamp; 
 	sread(&timestamp, sizeof(timestamp), 1);
-
 	printf("timestamp: %ld\n", timestamp);
+
+	int32_t data_len;
+	sread(&data_len, sizeof(data_len), 1);
+	printf("data_len: %d\n", data_len);
 }
 
 size_t read_osu_string(char *out_buffer)
@@ -115,4 +153,17 @@ uint64_t read_uleb128()
 	}
 
 	return result;
+}
+
+void u16_clendian(uint16_t *x)
+{
+	*x = (*x >> 8) | (*x << 8);
+}
+
+void u32_clendian(uint32_t *x)
+{
+	*x = (*x >> 24) | 
+		((*x << 8) & 0x00FF0000) |
+		((*x >> 8) & 0x0000FF00) |
+		(*x << 24);
 }
