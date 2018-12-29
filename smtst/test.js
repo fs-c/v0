79f2c6ba921efbedf1091c75af34240434a926d8
lw <me@lw.ag>
2018-12-29 20:18:45 +0100

    Push playground

--- smtst/test.js
+++ smtst/test.js
@@ -1,4 +1,13 @@
-const { serial, concurrent, execute } = require('./smtst');
+// const { serial, concurrent, execute } = require('./smtst');
+
+// serial.push({func: afunc, args: []}, {func: bfunc, args: []});
+
+// execute();
+
+const test = require('./smtst');
+const { execute } = test;
+
+test.context = this;
 
 const afunc = async () => {
     return 1;
@@ -8,6 +17,8 @@ const bfunc = async () => {
     throw new Error('Something went wrong');
 };
 
-serial.push({func: afunc, args: []}, {func: bfunc, args: []});
+(async () => {
+    await execute.serially(afunc);
+    await execute.serially(bfunc);
+})();
 
-execute();
