c5cf4277833c4584128f54bc205efc489d292430
lw <me@lw.ag>
2019-02-09 21:36:55 +0100

    Add potential mongodb integration

---
+++ node-steam-country/src/database.js
@@ -0,0 +1,43 @@
+/* Just leaving this here in case switching to MongoDB is ever on the table
+   again. */
+
+// const mongoose = require('mongoose');
+// const { database } = require('./constants');
+
+// exports.connect = mongoose.connect.bind(this,
+//     `mongodb+srv://${database.user}:${database.password}@${database.url}`);
+
+// const idSchema = new mongoose.Schema({
+//     ts: Number,
+//     open: Boolean,
+// });
+
+// idSchema.methods.close = () => {
+//     this.open = false;
+//     this.ts = Date.now();
+// };
+
+// idSchema.methods.open = () => {
+//     this.open = true;
+//     this.ts = Date.now();
+// };
+
+// idSchema.methods.sample = async () => {
+//     return await this.model.aggregate([{
+//         $sample: 1,
+//     }]);
+// };
+
+// const countrySchema = new mongoose.Schema({
+//     id: String,
+//     occ: Number,
+// });
+
+// countrySchema.methods.increase = () => {
+//     this.occ++;
+// };
+
+// exports.models = {
+//     Id: mongoose.model('Id', idSchema),
+//     Country: mongoose.model('Country', countrySchema),
+// };
