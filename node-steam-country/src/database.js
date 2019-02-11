f1b4bfedf55ae9646d41d62b1396beaec98a0af2
lw <me@lw.ag>
2019-02-11 22:18:49 +0100

    Remove unnecessary comments

--- node-steam-country/src/database.js
+++ node-steam-country/src/database.js
@@ -1,43 +1,43 @@
 /* Just leaving this here in case switching to MongoDB is ever on the table
    again. */
 
-// const mongoose = require('mongoose');
-// const { database } = require('./constants');
+const mongoose = require('mongoose');
+const { database } = require('./constants');
 
-// exports.connect = mongoose.connect.bind(this,
-//     `mongodb+srv://${database.user}:${database.password}@${database.url}`);
+exports.connect = mongoose.connect.bind(this,
+    `mongodb+srv://${database.user}:${database.password}@${database.url}`);
 
-// const idSchema = new mongoose.Schema({
-//     ts: Number,
-//     open: Boolean,
-// });
+const idSchema = new mongoose.Schema({
+    ts: Number,
+    open: Boolean,
+});
 
-// idSchema.methods.close = () => {
-//     this.open = false;
-//     this.ts = Date.now();
-// };
+idSchema.methods.close = () => {
+    this.open = false;
+    this.ts = Date.now();
+};
 
-// idSchema.methods.open = () => {
-//     this.open = true;
-//     this.ts = Date.now();
-// };
+idSchema.methods.open = () => {
+    this.open = true;
+    this.ts = Date.now();
+};
 
-// idSchema.methods.sample = async () => {
-//     return await this.model.aggregate([{
-//         $sample: 1,
-//     }]);
-// };
+idSchema.methods.sample = async () => {
+    return await this.model.aggregate([{
+        $sample: 1,
+    }]);
+};
 
-// const countrySchema = new mongoose.Schema({
-//     id: String,
-//     occ: Number,
-// });
+const countrySchema = new mongoose.Schema({
+    id: String,
+    occ: Number,
+});
 
-// countrySchema.methods.increase = () => {
-//     this.occ++;
-// };
+countrySchema.methods.increase = () => {
+    this.occ++;
+};
 
-// exports.models = {
-//     Id: mongoose.model('Id', idSchema),
-//     Country: mongoose.model('Country', countrySchema),
-// };
+exports.models = {
+    Id: mongoose.model('Id', idSchema),
+    Country: mongoose.model('Country', countrySchema),
+};
