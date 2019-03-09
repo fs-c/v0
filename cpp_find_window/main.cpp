11a6ac56e908e240db02ba2f065c9c19d70e7060
lw <me@lw.ag>
2019-03-09 10:01:20 +0100

    Add convenience logging function

--- cpp_find_window/main.cpp
+++ cpp_find_window/main.cpp
@@ -2,6 +2,11 @@
 
 #include <Windows.h>
 
+inline void log_win_err(const std::string &msg)
+{
+	std::cerr << msg.c_str() << " (" << GetLastError() << ")" << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2) {
@@ -14,12 +19,20 @@ int main(int argc, char *argv[])
 	std::string title {argv[1]};
 
 	HWND window = nullptr;
-	if (!FindWindowA(nullptr, title.c_str())) {
-		std::cerr << "couldn't find window (" << GetLastError() << ")"
-			<< std::endl;
+	if (!(window = FindWindowA(nullptr, title.c_str()))) {
+		log_win_err("couldn't find window");
 
 		return 1;
 	}
 
 	std::cout << "found window: " << window << std::endl;
+
+	char *test_title = nullptr;
+	if (!GetWindowTextA(window, test_title, 256)) {
+		log_win_err("couldn't fetch window title");
+
+		return 1;
+	}
+
+	std::cout << "window title: " << test_title << std::endl;
 }
