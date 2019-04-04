a24e32773cdb11eaf8a500224d9fe9bd7c6210c5
lw <root@fsoc.space>
2019-04-04 21:19:58 +0200

    Implement CaesarCipher

---
+++ misc/jku/se1/04/src/CaesarCipher.java
@@ -0,0 +1,56 @@
+class CaesarCipher {
+    public static final int UPCASE_BEGIN = 65;
+    public static final int LOCASE_BEGIN = 97;
+
+    public static void main(String[] args) {
+        // while (true) {
+        //     int c = (int)In.read();
+
+        //     Out.format("%d%n", c);
+        // }
+
+        while (true) {
+            Out.print("'e' for encrypt, 'd' for decrypt, 'q' to quit: ");
+            char mode = In.read();
+
+            if (mode == 'q' || (mode != 'd' && mode != 'e'))
+                return;
+
+            Out.print("Filename: ");
+            String output = In.readWord();
+
+            Out.print("Key: ");
+            int shift = (In.readInt() * (mode == 'd' ? -1 : 1)) % 26;
+
+            Out.print("Text: ");
+            String text = "";
+
+            In.read();
+
+            while ((int)In.peekRaw() != 10)
+                text += In.read();
+
+            StringBuilder cipher = new StringBuilder(text);
+
+            for (int i = 0; i < cipher.length(); i++) {
+                char old = cipher.charAt(i);
+
+                if (!Character.isLetter(old)) {
+                    cipher.setCharAt(i, old);
+                    continue;
+                }
+
+                int offset = Character.isUpperCase(old) ? UPCASE_BEGIN :
+                    LOCASE_BEGIN;
+
+                cipher.setCharAt(i, (char)(
+                    ((int)old - offset + shift) % 26 + offset
+                ));
+            }
+
+            Out.open(output);
+            Out.print(cipher);
+            Out.close();
+        }
+    }
+}
