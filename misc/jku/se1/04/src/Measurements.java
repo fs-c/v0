9d3f8cc1502693b5d09a9d7963283547acf69bbe
lw <root@fsoc.space>
2019-04-04 20:08:16 +0200

    Implement histogram

--- misc/jku/se1/04/src/Measurements.java
+++ misc/jku/se1/04/src/Measurements.java
@@ -7,13 +7,24 @@ class Measurements {
 		In.open(args[0]);
 		Out.open(args[1]);
 
+		int[] hist = new int[4];
 		double[] values = new double[VALUES_SIZE];
+
+		Out.println("Window means:");
 
 		int index = 0;
 		while (!In.isEof()) {
 			double cur = In.readDouble();
 
-			if (index < VALUES_SIZE) {
+			if (cur < 10.0) {
+				hist[0]++;
+			} else if (cur < 20.0) {
+				hist[1]++;
+			} else if (cur < 30.0) {
+				hist[2]++;
+			} else hist[3]++;
+
+			if (index < values.length) {
 				values[index++] = cur;
 
 				continue;
@@ -30,8 +41,12 @@ class Measurements {
 
 			mean /= values.length;
 
-			Out.println(String.format(Locale.US, "%.1f", mean));
+			Out.print(String.format(Locale.US, "%.1f ", mean));
 		}
+
+		Out.format("%nHistogram:%n[ 0.0, 10.0):%6d%n[10.0, 20.0):%6d"
+				+ "%n[20.0, 30.0):%6d%n[30.0, 50.0]:%6d", hist[0], hist[1],
+				hist[2], hist[3]);
 
 		In.close();
 		Out.close();
