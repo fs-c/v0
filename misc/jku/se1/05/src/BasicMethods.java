603fb8a6cd5531c4245ae34ec8c1bd6162d41f4e
lw <root@fsoc.space>
2019-04-09 16:31:58 +0200

    Add testing output

--- misc/jku/se1/05/src/BasicMethods.java
+++ misc/jku/se1/05/src/BasicMethods.java
@@ -1,41 +1,60 @@
+import java.util.Arrays; // For Arrays.toString() 
+
 class BasicMethods {
-    public static void main(String[] args) {
-        int value = -5;
-        int[] values = { 1, 2, 3, 4, -5 };
-        double[] floatingValues = { 1.1, 2.2, 3.3, 4.4 };
+	public static void main(String[] args) {
+		int value = -5;
+		int[] values = { 1, 2, 3, 4, -5 };
+		double[] floatingValues = { 1.1, 2.2, 3.3, 4.4 };
 
-        Out.format("abs: %d, min: %d, avg: %f, exists: %d%n", abs(value),
-            min(values), average(floatingValues), exists(values, value));
-    }
+		Out.format("|%d| = %d%n", value, abs(value));
 
-    static int abs(int x) {
-        return x < 0 ? x * -1 : x;
-    }
+		value = 50;
 
-    static int min(int[] values) {
-        int smallest = Integer.MAX_VALUE;
+		Out.format("|%d| = %d%n", value, abs(value));
 
-        for (int val : values)
-            if (val < smallest)
-                smallest = val;
-            
-        return smallest;
-    }
+		Out.format("min%s = %d%n", Arrays.toString(values), min(values));
+		Out.format("avg%s = %f%n", Arrays.toString(floatingValues),
+			average(floatingValues));
+		
+		value = -5;
 
-    static double average(double[] values) {
-        double sum = 0;
+		Out.format("%d is %sin %s%n", value,
+			exists(values, value) ? "" : "not ", Arrays.toString(values));
 
-        for (double val : values)
-            sum += val;
-        
-        return values.length == 0 ? 0 : sum / values.length;
-    }
+		value = 10;
+		
+		Out.format("%d is %sin %s%n", value,
+			exists(values, value) ? "" : "not ", Arrays.toString(values));
+	}
 
-    static boolean exists(int[] values, int x) {
-        for (int val : values)
-            if (val == x)
-                return true;
-        
-        return false;
-    }
+	static int abs(int x) {
+		return x < 0 ? x * -1 : x;
+	}
+
+	static int min(int[] values) {
+		int smallest = Integer.MAX_VALUE;
+
+		for (int val : values)
+			if (val < smallest)
+				smallest = val;
+
+		return smallest;
+	}
+
+	static double average(double[] values) {
+		double sum = 0;
+
+		for (double val : values)
+			sum += val;
+
+		return values.length == 0 ? 0 : sum / values.length;
+	}
+
+	static boolean exists(int[] values, int x) {
+		for (int val : values)
+			if (val == x)
+				return true;
+
+		return false;
+	}
 }
