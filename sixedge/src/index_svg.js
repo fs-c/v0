188c7d4c3eedebae6c71ff2bb11bc0b27e5a4a99
lw <root@fsoc.space>
2019-05-19 10:07:49 +0200

    Move to svg.js implementation to different file

--- sixedge/src/index_svg.js
+++ sixedge/src/index_svg.js
@@ -7,7 +7,7 @@ class Hexagon {
         this.poly = null;
         this.points = [];
 
-        const options = Object.assign({
+        const options = this.options = Object.assign({
             x: 0, y: 0, centered: false,
             size: 10,
         }, opts);
@@ -53,7 +53,7 @@ class Hexagon {
     }
 
     update() {
-        const radius = window.innerHeight / 100 * (this.size / 10);
+        const radius = this.radius = window.innerHeight / 100 * (this.size / 10);
         const height = (Math.sqrt(3) / 2) * radius;
 
         const points = new SVG.PointArray(this.calculatePoints(radius, height));
@@ -66,6 +66,20 @@ class Hexagon {
 
         this.poly.center(this.x, this.y).transform({ rotation: 0 })
             .fill({ opacity: 0 }).stroke({ color: '#f06', width: 2.5 });
+
+        if (this.options.text) {
+            if (!this.text) {
+                this.text = this.draw.text(this.options.text);
+            } else {
+                this.text.text(this.options.text);
+            }
+
+            this.text.font({
+                style: 'bold',
+                anchor: 'left',
+                size: this.radius / 3,
+            }).move(this.x - this.text.x() / 2, this.y - this.text.y() / 2);
+        }
     }
 }
 
@@ -79,5 +93,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const draw = SVG('container').size('100%', '100%');
 
-    const hex = new Hexagon(draw, { centered: true, size: 100 });
+    const hex = new Hexagon(draw, { centered: true, size: 100, text: 'HEXA\nNET' });
 });
