c3b930207bc32775ac1be058bdd552003af5c000
Laurenz Weixlbaumer <root@fsoc.space>
2020-02-07 20:59:14 +0100

    Push experimental maths

--- skips/src/app.js
+++ skips/src/app.js
@@ -1,34 +1,19 @@
-console.log(`alpha v0.0.1`);
+console.log(`alpha v0.1.0`);
+
+const TOTAL_WEEKS = 40;
+const SCHOOL_START = new Date(2019, 8, 9);
 
 const { parseCsv } = require('./csv');
 const { parseIcs } = require('./ics');
+const { readFile, daysIntoYear } = require('./utils');
 
 let currentAbsences = null;
 let currentTimetable = null;
 
 const csvInput = document.getElementById('csv-input');
 const icsInput = document.getElementById('ics-input');
+const resultBox = document.getElementById('result-box');
 const generateButton = document.getElementById('generate-button');
-
-const readFile = (file) => new Promise((resolve, reject) => {
-    const reader = new FileReader();
-
-    reader.addEventListener('load', () => {
-        resolve(reader.result);
-    });
-
-    reader.addEventListener('error', () => {
-        console.error('failed reading file', file);
-
-        reject(reader.error);
-    });
-
-    reader.addEventListener('progress', ({ loaded }) => {
-        console.log(`${loaded} bytes transferred`);
-    });
-
-    reader.readAsText(file, 'UTF-8');
-});
 
 const onFileSelected = (f) => async ({ target }) => {
     if (target.files.length > 1) {
@@ -65,8 +50,41 @@ const onIcsSelected = async (raw) => {
 
     console.log({ currentTimetable });
 };
-
 const onGenerateClick = async () => {
+    const subjects = {};
+    const absences = {};
+
+    for (const lesson of currentTimetable.flat()) {
+        subjects[lesson.subject] = (subjects[lesson.subject] || 0) + 1;
+    }
+
+    for (const absence of currentAbsences.absences) {
+        const lessons = currentTimetable[absence.day].filter((l) =>
+            l.begin.hour >= absence.begin.hour && l.end.hour <= absence.end.hour
+        );
+
+        if (!lessons.length) {
+            console.warn('no applicable lessons found for absence', absence);
+
+            continue;
+        }
+
+        for (const { subject } of lessons) {
+            absences[subject] = (absences[subject] || 0) + 1;
+        }
+    }
+
+    console.log({ absences, subjects });
+
+    const now = new Date();
+
+    console.log(daysIntoYear(SCHOOL_START), daysIntoYear(now), daysIntoYear(SCHOOL_START) + daysIntoYear(now), (daysIntoYear(SCHOOL_START) + daysIntoYear(now)) / 7);
+
+    resultBox.innerHTML = `${currentAbsences.absences.length} Absenzen analysiert\n`
+        + Object.keys(absences).map((e) =>
+            `${e}: ${absences[e]} - ${absences[e] / (subjects[e] * TOTAL_WEEKS)} (jhrl.)`
+            + ` - ${(absences[e] / (subjects[e] * ((365 - daysIntoYear(SCHOOL_START) -  + daysIntoYear(now)) / 7)))}`
+        ).join('\n');
 };
 
 csvInput.addEventListener('input', onFileSelected(onCsvSelected));
