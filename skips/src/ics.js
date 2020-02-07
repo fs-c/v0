d36ff501582b43c9bd70feba81f0ec9641998c29
Laurenz Weixlbaumer <root@fsoc.space>
2020-02-07 16:00:53 +0100

    Implement ICS parsing

---
+++ skips/src/ics.js
@@ -0,0 +1,48 @@
+const parseRawDate = (raw) => {
+    const date = raw.split('T')[0];
+    const time = raw.split('T')[1];
+
+    if (time.length !== 7 || time[time.length - 1] !== 'Z') {
+        throw new Error('unsupported time format, expected ISO 8601 UTC');
+    }
+
+    const year = date.slice(0, 4);
+    const month = parseInt(date.slice(4, 6)) - 1;
+    const day = date.slice(6, 8);
+
+    console.log({ year, month, day });
+
+    const hour = time.slice(0, 2);
+    const minute = time.slice(2, 4);
+    const second = time.slice(4, 6);
+
+    return new Date(year, month, day, hour, minute, second);
+};
+
+const parseIcs = async (raw) => {
+    const lines = raw.split('\n').map((e) => e.trim());
+
+    if (lines[0] !== 'BEGIN:VCALENDAR' || lines[2] !== 'VERSION:2.0') {
+        throw new Error('unsupported format or version');
+    }
+
+    const segments = [];
+    while (true) {
+        const index = lines.indexOf('BEGIN:VEVENT');
+
+        if (index === -1) {
+            break;
+        }
+
+        segments.push([ ...lines.splice(index, 8) ].splice(1));
+    }
+
+    return segments.map((e) => e.map((e) => e.split(':')[1])).map((e) => ({
+        start: parseRawDate(e[1]),
+        end: parseRawDate(e[2]),
+        day: parseRawDate(e[1]).getDay(),
+        subject: e[5],
+    }));
+};
+
+exports.parseIcs = parseIcs;
